723ffc2cb0f23c58415c5ca3b4132dfa39217a6e
Andrew Warren <anwarren@vbi.vt.edu>
2015-02-02 20:28:52 -0500

    updating to use dgrid table

--- public/js/p3/widget/app/Assembly.js
+++ public/js/p3/widget/app/Assembly.js
@@ -1,16 +1,38 @@
 define([
 	"dojo/_base/declare","dijit/_WidgetBase","dojo/on",
 	"dojo/dom-class","dijit/_TemplatedMixin","dijit/_WidgetsInTemplateMixin",
-	"dojo/text!./templates/Assembly.html","./AppBase"
+	"dojo/text!./templates/Assembly.html","./AppBase",
+	"dgrid/Grid","dgrid/extensions/DijitRegistry",
+        "dgrid/Keyboard", "dgrid/Selection","dgrid/extensions/ColumnResizer","dgrid/extensions/ColumnHider",
+        "dgrid/extensions/DnD","dojo/dnd/Source","dojo/_base/Deferred","dojo/aspect","dojo/_base/lang","dojo/domReady!"
 ], function(
 	declare, WidgetBase, on,
 	domClass,Templated,WidgetsInTemplate,
-	Template,AppBase
+	Template,AppBase,
+        Grid,Store,DijitRegistry,
+        Keyboard,Selection,formatter,ColumnResizer,
+        ColumnHider,DnD,DnDSource,
+        Deferred,aspect,lang,domReady
 ){
-	return declare([AppBase], {
+	return declare([Grid,DnD,Keyboard,ColumnResizer,DijitRegistry,AppBase], {
 		"baseClass": "App Assembly",
 		templateString: Template,
-		applicationName: "Assembly"
+		applicationName: "Assembly",
+		libraryData:[
+            { first: 'Bob', last: 'Barker', age: 89 },
+            { first: 'Vanna', last: 'White', age: 55 },
+            { first: 'Pat', last: 'Sajak', age: 65 }
+        ],
+		libraryGrid: new Grid({
+			columns: {'first': 'Libraries in assembly'},
+			},"librarytable"),
+                startup: function(){
+                        if (this._started) { return; }
+                        this.inherited(arguments);
+			this.libraryGrid.renderArray(this.libraryData);
+			this._started=true;
+			}
+			
 	});
 });
 
