91af808485a57e5b4ada313108f77c3188646c76
Andrew Warren <anwarren@vbi.vt.edu>
2015-02-02 23:50:44 -0500

    making table landing area

--- public/js/p3/widget/app/Assembly.js
+++ public/js/p3/widget/app/Assembly.js
@@ -19,6 +19,8 @@ define([
 		templateString: Template,
 		applicationName: "Assembly",
 		libraryData: null,
+		startingRows: 14,
+		addedRows: 0,
 		constructor: function(){
             		this.libraryData = [
 				{ first: 'Bob', last: 'Barker', age: 89 },
@@ -29,7 +31,11 @@ define([
                 startup: function(){
                         if (this._started) { return; }
                         this.inherited(arguments);
-
+			for (i = 0; i < this.startingRows; i++) { 
+				var tr =  this.libsTable.insertRow(0);//domConstr.create("tr",{},this.libsTableBody);
+				var td = domConstr.create('td', {innerHTML: "<div class='emptyrow'></div>"},tr);
+				var td2 = domConstr.create("td", {innerHTML: "<div class='emptyrow'></div>"},tr);
+			}
 /*
 			this.libraryGrid = new Grid({
 				columns: {'first': 'Libraries in assembly'}
@@ -43,13 +49,23 @@ define([
 
 		onAddPair: function(){
 			console.log("Create New Row", domConstr);
-			var tr =  domConstr.create("tr",{},this.libsTable);
+			var tr =  this.libsTable.insertRow(0);//var tr =  domConstr.create("tr",{},this.libsTableBody);
 			var td = domConstr.create('td', {innerHTML: "somefile.txt"},tr);
 			var td2 = domConstr.create("td", {innerHTML: "<i class='fa fa-times fa-1x' />"},tr);
+			if(this.addedRows < this.startingRows){
+				this.libsTable.deleteRow(-1);
+			}
 			var handle = on(td2, "click", function(evt){
 				domConstr.destroy(tr);
-				handle.remove();	
+				handle.remove();
+				this.addedRows = this.addedRows-1;
+				if (this.addedRows < this.startingRows){
+					var tr =  this.libsTable.insertRow(-1);//domConstr.create("tr",{},this.libsTable);
+					var td = domConstr.create('td', {innerHTML: "<div class='emptyrow'></div>"},tr);
+					var td2 = domConstr.create("td", {innerHTML: "<div class='emptyrow'></div>"},tr);
+				}	
 			});
+			this.addedRows= this.addedRows+1;
 		}
 		
 	});
