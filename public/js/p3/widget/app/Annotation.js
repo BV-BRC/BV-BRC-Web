8db710bc8e2d6e67a92a5afef3e14a7849cb09cc
Dustin Machi <dmachi@dojotoolkit.org>
2015-02-23 19:25:25 -0500

    add filtering select for taxon names

--- public/js/p3/widget/app/Annotation.js
+++ public/js/p3/widget/app/Annotation.js
@@ -1,11 +1,12 @@
 define([
 	"dojo/_base/declare","dijit/_WidgetBase","dojo/on",
 	"dojo/dom-class","dijit/_TemplatedMixin","dijit/_WidgetsInTemplateMixin",
-	"dojo/text!./templates/Annotation.html","./AppBase","p3/widget/WorkspaceFilenameValidationTextBox"
+	"dojo/text!./templates/Annotation.html","./AppBase","p3/widget/WorkspaceFilenameValidationTextBox",
+	"p3/widget/TaxonNameSelector"
 ], function(
 	declare, WidgetBase, on,
 	domClass,Templated,WidgetsInTemplate,
-	Template,AppBase
+	Template,AppBase,TaxonNameSelector
 ){
 	return declare([AppBase], {
 		"baseClass": "Annotation",
