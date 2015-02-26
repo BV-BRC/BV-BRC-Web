f6c84d4d5f91a30dc85f75027811a37566305c72
Dustin Machi <dmachi@dojotoolkit.org>
2015-02-26 09:02:25 -0500

    allow getObjectsBy type to peer into hidden folders

--- public/js/p3/widget/WorkspaceObjectSelector.js
+++ public/js/p3/widget/WorkspaceObjectSelector.js
@@ -265,7 +265,7 @@ define([
 
 		refreshWorkspaceItems: function(){
 			if (this._refreshing) { return; }
-			this._refreshing = WorkspaceManager.getObjectsByType(this.type,false).then(lang.hitch(this,function(items){
+			this._refreshing = WorkspaceManager.getObjectsByType(this.type,true).then(lang.hitch(this,function(items){
 				delete this._refreshing;
 				console.log("Ws Objects: ", items);
 				var store= new Memory({data: items,idProperty:"path"});
