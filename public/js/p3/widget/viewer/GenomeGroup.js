53cc607064e3a266d276640e73ba0761604a8fcb
Dustin Machi <dmachi@dojotoolkit.org>
2015-02-26 00:42:11 -0500

    selection to group

--- public/js/p3/widget/viewer/GenomeGroup.js
+++ public/js/p3/widget/viewer/GenomeGroup.js
@@ -1,11 +1,11 @@
 define([
 	"dojo/_base/declare","dijit/layout/BorderContainer","dojo/on",
 	"dojo/dom-class","dijit/layout/ContentPane","dojo/dom-construct",
-	"../PageGrid","../formatter","./GenomeList"
+	"../PageGrid","../formatter","../GenomeGrid"
 ], function(
 	declare, BorderContainer, on,
 	domClass,ContentPane,domConstruct,
-	Grid,formatter,GenomeList
+	Grid,formatter,GenomeGrid
 ){
 	return declare([BorderContainer], {
 		"baseClass": "GenomeGroup",
@@ -18,10 +18,19 @@ define([
 				this.viewer.set("query", query);
 			}
 		},
+		removeRows: function(ids){
+			ids.forEach(function(id){
+				var row = this.viewer.row(id);
+				this.viewer.removeRow(row.element);
+			},this);
+		},
+		refresh: function(){
+			this.viewer.refresh();
+		},
 		startup: function(){
 			if (this._started) {return;}
 			this.viewHeader = new ContentPane({content: "Genome Group Viewer", region: "top"});
-			this.viewer =  new GenomeList({
+			this.viewer =  new GenomeGrid({
 				region: "center",
 				query: (this.query||""),
 				apiToken: window.App.authorizationToken,
