cc951ac057a6d63f8c7b6bec21d28f82bb58a11a
Andrew Warren <anwarren@vbi.vt.edu>
2015-03-02 16:08:54 -0500

    modify Taxonomy name suggestor

--- public/js/p3/widget/app/Annotation.js
+++ public/js/p3/widget/app/Annotation.js
@@ -27,6 +27,13 @@ define([
 		onSuggestNameChange: function(val){
 			if (val && !this.output_nameWidget.get('value') || (this.output_nameWidget.get('value')&&this._selfSet)  ){
 				this._selfSet=true;
+				var sciName=this.scientific_nameWidget.get('displayedValue');
+				this.scientific_nameWidget.store.get(sciName).then(function(obj){
+					var tax_id=obj.taxon_id;
+					if(tax_id){
+						this.tax_idWidget.set('value',tax_id);
+					}
+				});
 				/*var abbrv=this.scientific_nameWidget.get('displayedValue');
 				abbrv=abbrv.match(/[^\s]+$/);
 				this.output_nameWidget.set('value',abbrv);*/
