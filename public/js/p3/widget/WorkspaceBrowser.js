ce2737bfc389ce90d1aaa908291d125d008ff6ac
Dustin Machi <dmachi@dojotoolkit.org>
2015-03-23 16:23:42 -0400

    fix genome browser link

--- public/js/p3/widget/WorkspaceBrowser.js
+++ public/js/p3/widget/WorkspaceBrowser.js
@@ -73,7 +73,7 @@ define([
 			},function(selection){
 				console.log("selection: ", selection);
 				var sel = selection[0];
-				window.location = "/portal/portal/patric/FeatureTable?cType=genome&featuretype=CDS&annotation=PATRIC&filtertype=&cId="+sel.genome_id
+				window.location = "/portal/portal/patric/GenomeBrowser?cType=genome&loc=0..10000&tracks=DNA,PATRICGenes,RefSeqGenescId=" + sel.genome_id
 			}, true);
 
 
