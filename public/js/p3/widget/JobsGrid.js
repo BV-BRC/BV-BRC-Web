5af7a04ce827b334c91c3d8886767a2f91440f5b
Dustin Machi <dmachi@dojotoolkit.org>
2015-05-28 13:07:28 -0400

    some more auth token changes

--- public/js/p3/widget/JobsGrid.js
+++ public/js/p3/widget/JobsGrid.js
@@ -250,6 +250,7 @@ define([
 					headers: {
 						"accept": "application/json",
 						"content-type": "application/json",
+						"Authorization": (window.app.authorizationToken||""),
 						'X-Requested-With': null
 					}
 				});
