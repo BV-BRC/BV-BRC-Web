3c31f6e228f3b694e4ffe27ac8bde9e6dbf3e2cb
Dustin Machi <dmachi@dojotoolkit.org>
2015-06-04 17:03:26 -0400

    don't try to autocomoplete tax id as it is a number

--- public/js/p3/widget/TaxIDSelector.js
+++ public/js/p3/widget/TaxIDSelector.js
@@ -35,7 +35,7 @@ define([
                         this.store.query = lang.hitch(this.store, function(query,options){
                                 console.log("query: ", query);
                                 console.log("Store Headers: ", _self.store.headers);
-                                var q = "?gt(" + _self.searchAttr + "," + query[_self.searchAttr] + ")";
+                                var q = "?eq(" + _self.searchAttr + "," + query[_self.searchAttr] + ")";
                                 if (_self.queryFilter) {
                                         q+=_self.queryFilter
                                 }
