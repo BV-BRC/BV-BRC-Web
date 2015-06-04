ce348d5854658ffce7d0bb91e537e180f4789bc8
Dustin Machi <dmachi@dojotoolkit.org>
2015-06-04 02:25:58 -0400

    release js

--- public/js/release/p3/widget/TaxIDSelector.js
+++ public/js/release/p3/widget/TaxIDSelector.js
@@ -1,9 +1,13 @@
 define("p3/widget/TaxIDSelector", [
 	"dijit/form/FilteringSelect","dojo/_base/declare",
-	"dojo/store/JsonRest"
+	"dojo/store/JsonRest","dojo/_base/lang","dojo/dom-construct",
+	"./TaxonNameSelector","dojo/on","dijit/TooltipDialog",
+	"dijit/popup"
 ], function(
 	FilteringSelect, declare, 
-	Store
+	Store,lang,domConstr,
+	TaxonNameSelector,on,TooltipDialog,
+	popup
 ){
 	
 	return declare([FilteringSelect], {
@@ -13,6 +17,8 @@ define("p3/widget/TaxIDSelector", [
 		placeHolder:'',
 		searchAttr: "taxon_id",
 		//query: "?&select(taxon_id)",
+		resultFields: ["taxon_id","taxon_name"],
+		sort: [{attribute: "taxon_id"}],
 		queryExpr: "${0}",
 		pageSize: 25,
 		autoComplete: false,
@@ -20,9 +26,47 @@ define("p3/widget/TaxIDSelector", [
 		required: false,
 
 		constructor: function(){
+			var _self=this;
 			if (!this.store){
 				this.store = new Store({target: this.apiServiceUrl + "/taxonomy/", idProperty: "taxon_id", headers: {accept: "application/json", "Authorization":(window.App.authorizationToken||"")}});
 			}
+                        var orig = this.store.query;
+                        this.store.query = lang.hitch(this.store, function(query,options){
+                                console.log("query: ", query);
+                                console.log("Store Headers: ", _self.store.headers);
+                                var q = "?gt(" + _self.searchAttr + "," + query[_self.searchAttr] + ")";
+                                if (_self.queryFilter) {
+                                        q+=_self.queryFilter
+                                }
+
+                                if (_self.resultFields && _self.resultFields.length>0) {
+                                        q += "&select(" + _self.resultFields.join(",") + ")";
+                                }
+				q += "&sort(+taxon_id)";
+                                console.log("Q: ", q);
+                                return orig.apply(_self.store,[q,options]);
+                        });
+		},
+
+		postCreate: function(){
+			var _self=this;
+			this.inherited(arguments);
+			this.filterButton = domConstr.create("i", {"class": "fa icon-search fa-1x", style: {"float":"right","font-size": "1.2em","margin": "2px"}});
+			domConstr.place(this.filterButton,this.domNode,"after");
+			var nameSearch = new TaxonNameSelector({});	
+			nameSearch.on("change", function(val){
+				_self.set('value', nameSearch.item[_self.searchAttr]);	
+			});
+			var searchTT=  new TooltipDialog({content: nameSearch.domNode, onMouseLeave: function(){ popup.close(searchTT); }})
+
+			on(this.filterButton, "click", lang.hitch(this,function() {
+	                     popup.open({
+                                        popup: searchTT,
+                                        around: this.domNode,
+                                        orient: ["below"]
+                                });
+			}));
+
 		},
 		/*
 		validate: function (){
@@ -30,6 +74,11 @@ define("p3/widget/TaxIDSelector", [
 			this.valueNode.value = this.toString();
 			return this.inherited(arguments);
 		}*/
+
+		labelFunc: function(item, store){
+			var label=item.taxon_id + " [" + item.taxon_name + "]";
+			return label;
+		},
 		isValid: function (){
 			// Overrides ValidationTextBox.isValid()
 			var error= !this.inherited(arguments);
