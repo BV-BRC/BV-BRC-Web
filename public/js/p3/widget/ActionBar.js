799d1551a5697db0d587d2dec7707b86e3ed161e
Dustin Machi <dmachi@dojotoolkit.org>
2015-08-19 13:32:46 -0400

    put a delay on select to prevent conflict with drill down clicking

--- public/js/p3/widget/ActionBar.js
+++ public/js/p3/widget/ActionBar.js
@@ -25,7 +25,7 @@ define([
 			this.set("selection", []);
 		},
 		_setSelectionAttr: function(sel){
-			console.log("setSelection", sel);
+			//console.log("setSelection", sel);
 			this.selection = sel;
 
 //			return;
@@ -33,7 +33,7 @@ define([
 			var selectionTypes = {}
 			sel.forEach(function(s){
 				var type = s.document_type || s.type;
-				console.log("Checking s: ", type, s);
+				//console.log("Checking s: ", type, s);
 				if (type=="job_result") {
 					if (s.autoMeta && s.autoMeta.app) {
 						if (typeof s.autoMeta.app=="string") {
@@ -43,21 +43,21 @@ define([
 						}
 					}
 				}
-				console.log("Type: ", type);
+				//console.log("Type: ", type);
 				selectionTypes[type]=true;
 			});
-			console.log("selectionTypes: ", selectionTypes);
+			//console.log("selectionTypes: ", selectionTypes);
 	
 			if (sel.length>1) {
 				var multiTypedSelection = (Object.keys(selectionTypes).length>1)?true:false;
 //				console.log("isMultiTyped: ", multiTypedSelection);	
 				valid = Object.keys(this._actions).filter(function(an){
-					console.log("Check action: ", an, this._actions[an].options);
+					//console.log("Check action: ", an, this._actions[an].options);
 					return this._actions[an] && this._actions[an].options && (this._actions[an].options.multiple && ((this._actions[an].options.ignoreDataType || !multiTypedSelection || (multiTypedSelection && this._actions[an].options.allowMultiTypes)) )||this._actions[an].options.persistent)
 				},this);	
 			
 
-				console.log("multiselect valid: ", valid)
+				//console.log("multiselect valid: ", valid)
 			}else if (sel.length==1){
 				valid = Object.keys(this._actions)
 			}else{
@@ -67,17 +67,17 @@ define([
 			}
 
 			var types = Object.keys(selectionTypes)
-			console.log("Filtering for Types: ", types);
+			//console.log("Filtering for Types: ", types);
 			valid = valid.filter(function(an){
 				var act = this._actions[an];
 				var validTypes = act.options.validTypes||[];
-				console.log("validTypes for action : ",an, validTypes);
+				//console.log("validTypes for action : ",an, validTypes);
 				var validContainerTypes = act.options.validContainerTypes || null;
 
 				if (validContainerTypes){
-					console.log("checkValidContainerTypes", validContainerTypes);
-					console.log("Current ContainerType: ", this.currentContainerType);
-					console.log("Current Container Widget: ", this.currentContainerWidget);
+					//console.log("checkValidContainerTypes", validContainerTypes);
+					//console.log("Current ContainerType: ", this.currentContainerType);
+					//console.log("Current Container Widget: ", this.currentContainerWidget);
 					if (!validContainerTypes.some(function(t){
 						return ((t=="*") || (t==this.currentContainerType))
 					},this)){
@@ -90,7 +90,7 @@ define([
 				});		
 			},this);
 
-			console.log("ValidTypes: ", valid);
+			//console.log("ValidTypes: ", valid);
 			Object.keys(this._actions).forEach(function(an){
 				var act = this._actions[an];
 				if (valid.indexOf(an)>=0){
@@ -107,14 +107,14 @@ define([
 			var _self=this;
 			this.containerNode=this.domNode;
 			on(this.domNode, ".ActionButtonWrapper:click", function(evt){
-				console.log("evt.target: ", evt.target);
+				//console.log("evt.target: ", evt.target);
 				var target;
 				if (evt && evt.target && evt.target.attributes && evt.target.attributes.rel) {
 					target = evt.target;
 				}else{
 					target = evt.target.parentNode;
 				}
-				console.log("target: ", target);
+				//console.log("target: ", target);
 				if (target && target.attributes && target.attributes.rel) {	
 					var rel = target.attributes.rel.value;
 					if (_self._actions[rel]) {
@@ -124,17 +124,17 @@ define([
 			});	
 
 //			on(this.domNode, ".ActionButton:mouseover", function(evt){
-//				console.log("mouseover evt: ", evt.target);
+//				//console.log("mouseover evt: ", evt.target);
 //			});	
 			new Tooltip({
 				connectId: this.domNode,
 				selector: ".ActionButtonWrapper",
 				getContent: function(matched){
-					console.log("Matched: ", matched);
+					//console.log("Matched: ", matched);
 					var rel = matched.attributes.rel.value;
-					console.log("REL: ", rel);
+					//console.log("REL: ", rel);
 					if (_self._actions[rel] && _self._actions[rel].options && _self._actions[rel].options.tooltip){
-						console.log("_self._actions[rel]:", rel, _self._actions[rel]);
+						//console.log("_self._actions[rel]:", rel, _self._actions[rel]);
 						return _self._actions[rel].options.tooltip
 					}else if (matched.attributes.title && matched.attributes.title.value){
 						return  matched.attributes.title.value;
