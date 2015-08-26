fa99e8dd7e312cf34f2ae4c1c1b6b1eb96d59060
Andrew Warren <anwarren@vbi.vt.edu>
2015-08-26 16:31:20 -0400

    updating annotation interface to have simpler naming options

--- public/js/p3/widget/TaxonNameSelector.js
+++ public/js/p3/widget/TaxonNameSelector.js
@@ -10,8 +10,14 @@ define([
 		apiServiceUrl: window.App.dataAPI,
 		promptMessage:'Scientific name of the organism being annotated.',
 		missingMessage:'Scientific Name must be provided.',
-		placeHolder:'e.g. Bacillus Cereus',
+		placeHolder:'e.g. species Bacillus Cereus',
 		searchAttr: "taxon_name",
+        resultFields: ["taxon_name","taxon_id","taxon_rank", "lineage_names"],
+        rankAttrs: ["taxon_rank"],
+        subStringAttrs: ["taxon_name"],
+        promoteAttrs: ["taxon_id^3","taxon_rank^2"],
+        intAttrs:["taxon_id"],
+        rankList:["species","no rank","genus","subspecies","family","order","class","phylum","species group","suborder","varietas","species subgroup","subclass","subgenus","forma","superphylum","superkingdom","tribe","subfamily","subphylum"],
 		//query: "?&select(taxon_name)",
 		queryExpr: "*${0}*",
 		pageSize: 25,
@@ -19,12 +25,86 @@ define([
 		autoComplete: false,
 		store: null,
 		constructor: function(){
+            this.constructorSOLR();
+			//if (!this.store){
+			//	this.store = new Store({target: this.apiServiceUrl + "/taxonomy/", idProperty: "taxon_id", headers: {accept: "application/json"}});
+			//}
+		},
+		constructorSOLR: function(){
+			var _self=this;
 			if (!this.store){
-				this.store = new Store({target: this.apiServiceUrl + "/taxonomy/", idProperty: "taxon_id", headers: {accept: "application/json"}});
+				this.store = new Store({target: this.apiServiceUrl + "/taxonomy/", idProperty: "taxon_id", headers: {accept: "application/json", "content-type": "application/solrquery+x-www-form-urlencoded"}});
 			}
+
+			var orig = this.store.query;
+			this.store.query = lang.hitch(this.store, function(query,options){
+				console.log("query: ", query);
+				console.log("Store Headers: ", _self.store.headers);
+				var q = "?q=";
+                var extraSearch=[];
+                var qString=query[_self.searchAttr].toString().replace(/\.\*|\[|\]/g,'');
+
+                var rankParts=[];
+                _self.rankList.forEach(function(rank){
+                    var re = new RegExp("(\\b)"+rank+"(\\b)","gi");
+                    var newQString = qString.replace(re, "");
+                    if (newQString != qString){
+                        rankParts.push(rank);
+                        qString= newQString.trim();
+                    }
+                });
+                
+                var queryParts= qString ? qString.split(/[ ,]+/) : [];
+
+                rankParts.forEach(function(qPart){
+                    _self.rankAttrs.forEach(function(item){
+                        extraSearch.push('('+item + ':' + qPart + ')');
+                    });
+                });
+
+                queryParts.forEach(function(qPart){
+                    _self.intAttrs.forEach(function(item){
+                        if(!isNaN(qPart) && qPart){ //only if its a number
+                            extraSearch.push("("+item + ":" + qPart + ")");
+                        }
+                    });
+                    _self.subStringAttrs.forEach(function(item){
+                        if (qPart.length > 1){
+                            extraSearch.push("("+item + ":" + qPart + ")"); //for this attribute value an exact match valued more
+                            extraSearch.push("("+item + ":*" + qPart + "*)");
+                        }
+                    });
+                });
+                if(queryParts.length){
+                    _self.subStringAttrs.forEach(function(item){
+                        extraSearch.push("("+item + ":*" + queryParts.join('*') + "*)");
+                    });
+                }
+
+                q+="("+extraSearch.join(' OR ')+")";
+
+				if (_self.queryFilter) {
+					q+=_self.queryFilter
+				}
+
+				if (_self.resultFields && _self.resultFields.length>0) {
+					q += "&fl=" + _self.resultFields.join(",");
+				}
+				if (_self.promoteAttrs && _self.promoteAttrs.length>0) {
+					q += '&qf="' + _self.promoteAttrs.join(" ")+'"';
+				}
+                //var re = new RegExp("\\s+","gi");
+                //q=q.replace(re,"+"); //hack appropriate web api handling spaces
+				console.log("Q: ", q);
+				return orig.apply(_self.store,[q,options]);
+			});	
 		},
 		isValid: function(){
 			return (!this.required || this.get('displayedValue') != "");
+		},
+		labelFunc: function(item, store){
+			var label="["+item.taxon_rank+"] "  + item.taxon_name;
+			return label;
 		},
 
 		_setDisplayedValueAttr: function(/*String*/ label, /*Boolean?*/ priorityChange){
