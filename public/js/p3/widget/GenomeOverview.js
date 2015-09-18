5a35da8e7496b4eed05b07ab7f96246d53cd2f2b
Harry Yoo <hyun@vbi.vt.edu>
2015-09-18 16:21:05 -0400

    format corrected

--- public/js/p3/widget/GenomeOverview.js
+++ public/js/p3/widget/GenomeOverview.js
@@ -4,10 +4,10 @@ define([
 	"dojo/request", "dojo/_base/lang", "dojox/charting/Chart2D", "dojox/charting/themes/ThreeD", "dojox/charting/action2d/MoveSlice",
 	"dojox/charting/action2d/Tooltip", "dojo/dom-construct"
 
-], function (declare, WidgetBase, on,
-			 domClass, Templated, Template,
-			 xhr, lang, Chart2D, Theme, MoveSlice,
-			 ChartTooltip, domConstruct) {
+], function(declare, WidgetBase, on,
+			domClass, Templated, Template,
+			xhr, lang, Chart2D, Theme, MoveSlice,
+			ChartTooltip, domConstruct) {
 	return declare([WidgetBase, Templated], {
 		baseClass: "GenomeOverview",
 		disabled: false,
@@ -15,7 +15,7 @@ define([
 		apiServiceUrl: window.App.dataAPI,
 		genome: null,
 		genomeFeatureSummary: null,
-		"_setGenomeAttr": function (genome) {
+		"_setGenomeAttr": function(genome) {
 			this.genome = genome;
 			console.log("Set Genome", genome);
 
@@ -25,7 +25,7 @@ define([
 				this.refresh();
 			}
 		},
-		"_setGenomeFeatureSummaryAttr": function (summary) {
+		"_setGenomeFeatureSummaryAttr": function(summary) {
 			domConstruct.empty(this.gfDataNode);
 			var table = domConstruct.create("table", {}, this.gfDataNode);
 			var tbody = domConstruct.create("tbody", {}, table);
@@ -35,7 +35,7 @@ define([
 			domConstruct.create("td", {innerHTML: "PATRIC"}, htr);
 			domConstruct.create("td", {innerHTML: "RefSeq"}, htr);
 
-			Object.keys(summary).forEach(function (prop) {
+			Object.keys(summary).forEach(function(prop) {
 				var tr = domConstruct.create('tr', {}, tbody);
 				domConstruct.create("td", {innerHTML: prop}, tr);
 				domConstruct.create("td", {innerHTML: summary[prop]}, tr);
@@ -62,9 +62,9 @@ define([
 			 */
 		},
 
-		"_setProteinFeatureSummaryAttr": function (summary) {
+		"_setProteinFeatureSummaryAttr": function(summary) {
 			var gfData = [];
-			Object.keys(summary).forEach(function (prop) {
+			Object.keys(summary).forEach(function(prop) {
 				gfData.push({text: prop, x: 0, y: summary[prop]});
 			});
 			console.log("GF DATA: ", gfData);
@@ -86,7 +86,7 @@ define([
 
 			this.pfPieChart.render();
 		},
-		"_setSpecialtyGeneSummaryAttr": function (summary) {
+		"_setSpecialtyGeneSummaryAttr": function(summary) {
 			domConstruct.empty(this.sgDataNode);
 			var table = domConstruct.create("table", {}, this.sgDataNode);
 			var tbody = domConstruct.create("tbody", {}, table);
@@ -96,7 +96,7 @@ define([
 			domConstruct.create("td", {innerHTML: "Source"}, htr);
 			domConstruct.create("td", {innerHTML: "Genes"}, htr);
 
-			Object.keys(summary).forEach(function (prop) {
+			Object.keys(summary).forEach(function(prop) {
 				var tr = domConstruct.create('tr', {}, tbody);
 				var property_source = prop.split(':');
 				domConstruct.create("td", {innerHTML: property_source[0]}, tr);
@@ -105,12 +105,12 @@ define([
 			});
 		},
 
-		getSummaryData: function () {
+		getSummaryData: function() {
 			// getting genome feature summary
 			xhr.get(this.apiServiceUrl + "/genome_feature/?eq(genome_id," + this.genome.genome_id + ")&limit(1)&facet((field,feature_type),(mincount,1))&http_accept=application/solr+json", {
 				handleAs: "json",
 				headers: {"accept": "application/solr+json"}
-			}).then(lang.hitch(this, function (data) {
+			}).then(lang.hitch(this, function(data) {
 				console.log("Data: ", data);
 				var genomeSummary = {};
 				var idx = 0;
@@ -126,7 +126,7 @@ define([
 				handleAs: "json",
 				headers: {"accept": "application/solr+json"}
 			})
-				.then(lang.hitch(this, function (data) {
+				.then(lang.hitch(this, function(data) {
 					console.log("Data: ", data);
 					var spSummary = {};
 					var idx = 0;
@@ -139,9 +139,9 @@ define([
 				}));
 
 		},
-		refresh: function () {
+		refresh: function() {
 			if(this.genome && this.genome.genome_id){
-				Object.keys(this.genome).forEach(lang.hitch(this, function (prop) {
+				Object.keys(this.genome).forEach(lang.hitch(this, function(prop) {
 					if(this[prop + "Node"]){
 						this[prop + "Node"].innerHTML = this.genome[prop];
 					}
@@ -152,7 +152,7 @@ define([
 				console.log("Invalid Genome: ", this.genome);
 			}
 		},
-		startup: function () {
+		startup: function() {
 			if(this._started){
 				return;
 			}
