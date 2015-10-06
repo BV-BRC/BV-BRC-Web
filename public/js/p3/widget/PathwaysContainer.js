fe0030796025d014009e68f24af0235af0bfaf26
Dustin Machi <dmachi@dojotoolkit.org>
2015-10-06 14:44:27 -0400

    add pathwaysgridcontainer so individual tabs have their own action controllers

--- public/js/p3/widget/PathwaysContainer.js
+++ public/js/p3/widget/PathwaysContainer.js
@@ -1,7 +1,7 @@
 define([
 	"dojo/_base/declare", "dijit/layout/BorderContainer", "dojo/on","dojo/_base/lang",
 	"./ActionBar", "./ContainerActionBar", "dijit/layout/StackContainer", "dijit/layout/TabController",
-	"./PathwaysGrid", "dijit/layout/ContentPane","./GridContainer","dijit/TooltipDialog",
+	"./PathwaysGridContainer", "dijit/layout/ContentPane","./GridContainer","dijit/TooltipDialog",
 	"./ItemDetailPanel","dojo/topic","dijit/form/ToggleButton"
 ], function(declare, BorderContainer, on, lang,
 			ActionBar, ContainerActionBar, TabContainer,StackController,
@@ -46,6 +46,7 @@ define([
 				if(params.annotation != null) q.push('annotation:' + params.annotation);
 				if(params.pathway_id != null) q.push('pathway_id:' + prarms.pathway_id);
 
+
 				this.pathwaysGrid.set("params", params);
 
 				/*
@@ -58,76 +59,6 @@ define([
 				});
 				*/
 			}
-		},
-		listen: function(){
-			this.pathwaysGrid.on("select", lang.hitch(this,function(evt){
-				console.log("Selected: ", evt);
-				var sel = Object.keys(evt.selected).map(lang.hitch(this,function(rownum){
-					console.log("rownum: ", rownum);
-					console.log("Row: ", evt.grid.row(rownum).data);
-					return evt.grid.row(rownum).data;
-				}));
-				console.log("selection: ", sel);
-				this.selectionActionBar.set("selection", sel);
-				this.itemDetailPanel.set('selection', sel);
-			}));	
-
-			this.pathwaysGrid.on("deselect", lang.hitch(this,function(evt){
-
-				if (!evt.selected) { 
-					this.actionPanel.set("selection", []); 
-					this.itemDetailPanel.set("selection", []);
-				}else{
-					var sel = Object.keys(evt.selected).map(lang.hitch(this,function(rownum){
-						console.log("rownum: ", rownum);
-						console.log("Row: ", evt.grid.row(rownum).data);
-						return evt.grid.row(rownum).data;
-					}));
-				}
-				console.log("selection: ", sel);
-				this.selectionActionBar.set("selection", sel);
-				this.itemDetailPanel.set('selection', sel);
-			}));
-		},
-		containerActions: GridContainer.prototype.containerActions.concat([
-			[
-				"DownloadTable",
-				"fa fa-download fa-2x",
-				{label:"DOWNLOAD",multiple: false,validTypes:["*"],tooltip: "Download Table", tooltipDialog:downloadTT}, 
-				function(selection){	
-					popup.open({
-						popup: this.containerActionBar._actions.DownloadTable.options.tooltipDialog,
-						around: this.containerActionBar._actions.DownloadTable.button,
-						orient: ["below"]
-					});
-				},
-				true
-			]
-		]),
-		selectionActions: GridContainer.prototype.selectionActions.concat([
-			[
-				"ViewFASTA",
-				"fa icon-fasta fa-2x",
-				{label: "FASTA",ignoreDataType:true, multiple: true,validTypes:["*"], tooltip: "View FASTA Data",tooltipDialog:viewFASTATT},
-				function(selection){
-					popup.open({
-						popup: this.selectionActionBar._actions.ViewFASTA.options.tooltipDialog,
-						around: this.selectionActionBar._actions.ViewFASTA.button,
-						orient: ["below"]
-					});
-				},
-				false
-			]
-
-		]),
-		setupActions: function(){
-			this.containerActions.forEach(function(a){
-				this.containerActionBar.addAction(a[0],a[1],a[2],lang.hitch(this,a[3]),a[4]);
-			}, this);
-
-			this.selectionActions.forEach(function(a){
-				this.selectionActionBar.addAction(a[0],a[1],a[2],lang.hitch(this,a[3]),a[4]);
-			}, this);
 		},
 
 		visible: false,
@@ -151,21 +82,6 @@ define([
 				return;
 			}
 
-			this.containerActionBar = new ContainerActionBar({
-				region: "top",
-				splitter: false,
-				layoutPriority: 2,
-				"className": "BrowserHeader"
-			});
-
-			this.selectionActionBar = new ActionBar({
-				region: "right",
-				layoutPriority: 2,
-				style: "width:48px;text-align:center;",
-				splitter: false
-			});
-
-			this.itemDetailPanel = new ItemDetailPanel({region: "right", style: "width:300px", splitter: false, layoutPriority:1});
 			this.tabContainer = new TabContainer({region: "center", id: this.id + "_TabContainer"});
 			var tabController = new StackController({containerId: this.id + "_TabContainer", region: "top", "class": "TextTabButtons"})
 
@@ -176,16 +92,12 @@ define([
 			this.ecNumbersGrid = new ContentPane({title: "EC Numbers", content: "EC Numbers Grid"});
 			this.genesGrid = new ContentPane({title: "Genes", content: "Genes Grid"});
 
-			// this.addChild(this.containerActionBar);
 			this.addChild(tabController);
 			this.addChild(this.tabContainer);
-			// this.addChild(this.selectionActionBar);
 			this.tabContainer.addChild(this.pathwaysGrid);
 			this.tabContainer.addChild(this.ecNumbersGrid);
 			this.tabContainer.addChild(this.genesGrid);
 
-			this.listen();
-			this.setupActions();
 			this.inherited(arguments);
 		}
 	});
---
+++ public/js/p3/widget/PathwaysGridContainer.js
@@ -0,0 +1,128 @@
+define([
+	"dojo/_base/declare", "./GridContainer","dojo/on",
+	"./PathwaysGrid","dijit/popup","dojo/topic",
+	"dijit/TooltipDialog","./FacetFilterPanel",
+	"dojo/_base/lang","dojo/on"
+
+], function(
+	declare, GridContainer,on,
+	PathwaysGrid,popup,Topic,
+	TooltipDialog,FacetFilterPanel,
+	lang,on
+){
+
+	var vfc = '<div class="wsActionTooltip" rel="dna">View FASTA DNA</div><div class="wsActionTooltip" rel="protein">View FASTA Proteins</div><hr><div class="wsActionTooltip" rel="dna">Download FASTA DNA</div><div class="wsActionTooltip" rel="downloaddna">Download FASTA DNA</div><div class="wsActionTooltip" rel="downloadprotein"> '
+	var viewFASTATT=  new TooltipDialog({content: vfc, onMouseLeave: function(){ popup.close(viewFASTATT); }})
+
+	var dfc = '<div>Download Table As...</div><div class="wsActionTooltip" rel="text/tsv">Text</div><div class="wsActionTooltip" rel="text/csv">CSV</div><div class="wsActionTooltip" rel="application/vnd.openxmlformats">Excel</div>'
+	var downloadTT=  new TooltipDialog({content: dfc, onMouseLeave: function(){ popup.close(downloadTT); }})
+
+	on(downloadTT.domNode, "div:click", function(evt){
+		var rel = evt.target.attributes.rel.value;
+		console.log("REL: ", rel);
+		var selection = self.actionPanel.get('selection')
+		var dataType=(self.actionPanel.currentContainerWidget.containerType=="genome_group")?"genome":"genome_feature"
+		var currentQuery = self.actionPanel.currentContainerWidget.get('query');
+		console.log("selection: ", selection);
+		console.log("DownloadQuery: ", dataType, currentQuery );
+		window.open("/api/" + dataType + "/" + currentQuery + "&http_authorization=" + encodeURIComponent(window.App.authorizationToken) + "&http_accept=" + rel + "&http_download");		
+		popup.close(downloadTT);
+	});
+
+	return declare([GridContainer],{
+		gridCtor: PathwaysGrid,
+		facetFields: ["annotation","feature_type"],
+
+		_setParamsAttr: function(params) {
+			this.params = params;
+			if(this._started && this.grid){
+				var q = [];
+				if(params.genome_id != null) q.push('genome_id:' + params.genome_id);
+				if(params.annotation != null) q.push('annotation:' + params.annotation);
+				if(params.pathway_id != null) q.push('pathway_id:' + prarms.pathway_id);
+
+				this.grid.set("params", params);
+			}
+		},
+
+		containerActions: GridContainer.prototype.containerActions.concat([
+			[
+				"DownloadTable",
+				"fa fa-download fa-2x",
+				{label:"DOWNLOAD",multiple: false,validTypes:["*"],tooltip: "Download Table", tooltipDialog:downloadTT}, 
+				function(selection){	
+					popup.open({
+						popup: this.containerActionBar._actions.DownloadTable.options.tooltipDialog,
+						around: this.containerActionBar._actions.DownloadTable.button,
+						orient: ["below"]
+					});
+				},
+				true
+			]
+		]),
+		selectionActions: GridContainer.prototype.selectionActions.concat([
+			[
+				"ViewFASTA",
+				"fa icon-fasta fa-2x",
+				{label: "FASTA",ignoreDataType:true, multiple: true,validTypes:["*"], tooltip: "View FASTA Data",tooltipDialog:viewFASTATT},
+				function(selection){
+					popup.open({
+						popup: this.selectionActionBar._actions.ViewFASTA.options.tooltipDialog,
+						around: this.selectionActionBar._actions.ViewFASTA.button,
+						orient: ["below"]
+					});
+				},
+				false
+			]
+
+		]),
+		getFilterPanel: function(){
+			return false;
+			if (!this.filterPanel) { 
+				this.filterPanel = new FacetFilterPanel({style: "color:#fff;",facets: {} });
+
+				this.filterPanel.watch("filter", lang.hitch(this,function(attr,oldValue,newValue){
+					on.emit(this.domNode, "UpdateHash", {bubbles: true, cancelable: true, hashProperty: "filter", value: newValue, oldValue: oldValue} )
+				}))
+			}
+			this.filterPanel.clearFilters();
+			return this.filterPanel 
+		},
+
+		filter: null,
+
+		_setFilterAttr: function(filter){
+			this.inherited(arguments);
+
+			if (filter){
+				console.log("Parsing hashParams Filter: ", this.hashParams.filter);
+				var re = /(eq\(\w+\,\w+\))+/gi;
+				var innerRE = /eq\(\w+\,\w+\)/
+				var matches = filter.match(re);
+				console.log("Matches: ", matches);
+				var selected = matches.map(function(match){
+					var parts = match.replace("eq(","").replace(/\)$/,"").split(",");
+					return parts[0] +":"+ parts[1];
+				})
+
+				console.log("Selected: ", selected);
+				if (this.filterPanel){
+						this.filterPanel.set('selected', selected);
+				}
+			}
+		},
+		startup: function(){
+			if (this._started) { return; }			
+			var _self=this;
+			this.inherited(arguments);
+
+			/*
+			this.grid.store.on("facet_counts", function(evt){
+				if (_self.filterPanel){
+					_self.filterPanel.set("facets", evt.facet_counts.facet_fields);
+				}
+			})	
+			*/
+		}
+	});
+});
