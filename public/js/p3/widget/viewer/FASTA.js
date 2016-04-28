de42451be7d2d97cc9c93a287350307da8bf179b
Dustin Machi <dmachi@dojotoolkit.org>
2016-04-28 16:18:21 -0400

    when navigating to a url > 4k chars, strip the query off but still pass it in the search property of the state object

--- public/js/p3/widget/viewer/FASTA.js
+++ public/js/p3/widget/viewer/FASTA.js
@@ -1,8 +1,8 @@
 define([
 	"dojo/_base/declare", "dijit/layout/ContentPane", "./Base",
-	"dojo/request", "../../util/PathJoin"
+	"dojo/request/xhr", "../../util/PathJoin","dojo/when"
 ], function(declare, ContentPane, Base,
-			Request, PathJoin){
+			Request, PathJoin, when){
 	return declare([Base], {
 		dataModel: "genome_feature",
 		limit: 25000,
@@ -18,13 +18,44 @@ define([
 				return;
 			}
 			var query = state.search;
-			query = query + "&http_accept=application/" + type + "+fasta&limit(" + this.limit + ")";
 
-			console.log("FASTA Query URL : ", PathJoin(this.apiServiceUrl, this.dataModel, "?" + query));
+			if (this.contentPanel){
+				this.contentPanel.set("content", "Loading FASTA Data...");
+			}
+			var _self=this;
 
-			this.contentPanel.set("href", PathJoin(this.apiServiceUrl, this.dataModel, "?" + query));
-
+			when(this.getData(type, query), function(data){
+				// console.log("Got FASTA Data");
+				_self.contentPanel.set("content", data);
+			}, function(err){
+				_self.contentPanel.set("content", "There was an error retrieving the requested data: " + err);
+			})
 		},
+
+		getData: function(type,query){
+
+
+			// console.log("Query Data Length: ", query.length);
+
+			query = query + "&limit(" + this.limit + ")";
+			url = PathJoin(this.apiServiceUrl, this.dataModel) + "/"
+			// console.log("FASTA getData POST URL: ", url);
+			// console.log("POST DATA: ", query);
+			var _self=this;
+			
+			return Request.post(url, {
+				method: "post",
+				headers: {
+				 "Content-Type": "application/rqlquery+x-www-form-urlencoded",
+				 "accept": "application/" + type + "+fasta",
+				 'authorization': (window.App.authorizationToken || ""),
+				 'X-Requested-With': null
+				},		 
+                'X-Requested-With': null,
+				data: query
+			})
+		},
+
 		startup: function(){
 			var query;
 			if(this._started){
@@ -37,8 +68,8 @@ define([
 				if(parts && (parts.length > 2) && parts[parts.length - 2]){
 					var type = parts[parts.length - 2];
 				}
-				query = query + "&http_accept=application/" + type + "+fasta&limit(" + this.limit + ")";
-				query = PathJoin(this.apiServiceUrl, this.dataModel, "?" + query);
+				// query = query + "&http_accept=application/" + type + "+fasta&limit(" + this.limit + ")";
+				// query = PathJoin(this.apiServiceUrl, this.dataModel, "?" + query);
 			}
 
 			this.header = new ContentPane({
@@ -49,13 +80,18 @@ define([
 
 			this.contentPanel = new ContentPane({
 				region: "center",
-				href: (query ? query : ""),
+				// href: (query ? query : ""),
+				content: "Loading FASTA Data...",
 				style: "word-wrap:break-word;font-family:monospace;white-space:pre;margin:1em;font-size:1.1em;"
 			});
 			this.addChild(this.header);
 			this.addChild(this.contentPanel);
 
 			this.inherited(arguments);
+
+			if (this.state){
+				this.onSetState("state",this.state,this.state);
+			}
 		}
 	})
 });
