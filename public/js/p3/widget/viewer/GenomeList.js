bc33998c5f492f07506c48575ad2a69f354e1613
Harry Yoo <hsyoo@anl.gov>
2016-06-27 10:24:51 -0500

    cleanup genome group / genome list landing page

--- public/js/p3/widget/viewer/GenomeList.js
+++ public/js/p3/widget/viewer/GenomeList.js
@@ -1,5 +1,16 @@
 define([
-	"dojo/_base/declare", "./_GenomeList"
-], function(declare, GenomeList){
-	return declare([GenomeList], {});
+	"dojo/_base/declare", "dijit/layout/ContentPane", "./_GenomeList"
+], function(declare, ContentPane, GenomeList){
+	return declare([GenomeList], {
+		onSetQuery: function(attr, oldVal, newVal){
+			// prevent default action
+		},
+		createOverviewPanel: function(){
+			return new ContentPane({
+				content: "Genome List Overview",
+				title: "Overview",
+				id: this.viewer.id + "_" + "overview"
+			})
+		}
+	});
 });
