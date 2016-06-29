2dea9d3b4b10802a41029745bb811ae5adb59219
Dustin Machi <dmachi@dojotoolkit.org>
2016-06-29 12:29:50 -0400

    hack in fix for transcriptomics viewer in feature tab

--- public/js/p3/widget/GeneExpressionContainer.js
+++ public/js/p3/widget/GeneExpressionContainer.js
@@ -34,6 +34,7 @@ define([
 		},
 		onSetState: function(attr, oldVal, state){
 			console.log("GeneExpressionGridContainer onSetState set state: ", state);
+			if (!state){ return; }
 			if(this.GeneExpressionGridContainer){
 				this.GeneExpressionGridContainer.set('state', state);
 			}
