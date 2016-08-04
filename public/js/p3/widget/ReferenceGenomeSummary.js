45e45fa5ceb3ab80f9e0657b19299c673954f0ef
Harry Yoo <hsyoo@anl.gov>
2016-08-04 15:16:16 -0500

    style update, add links to graph slice

--- public/js/p3/widget/ReferenceGenomeSummary.js
+++ public/js/p3/widget/ReferenceGenomeSummary.js
@@ -1,17 +1,13 @@
 define([
 	"dojo/_base/declare", "dojo/_base/lang",
-	"dojo/dom-class", "dojo/dom-construct", "dojo/on", "dojo/request",
-	"dojo/fx/easing",
-	"dijit/_WidgetBase",
-	"dojox/charting/Chart2D", "./PATRICTheme", "dojox/charting/action2d/MoveSlice", "dojox/charting/plot2d/Pie", "dojox/charting/action2d/Tooltip",
-	"dojox/charting/plot2d/Bars", "./SummaryWidget"
+	"dojo/dom-class", "dojo/dom-construct", "dojo/on", "dojo/request", "dojo/topic",
+	"dojox/charting/Chart2D", "./PATRICTheme", "dojox/charting/plot2d/Pie",
+	"./SummaryWidget"
 
 ], function(declare, lang,
-			domClass, domConstruct, on, xhr,
-			easing,
-			WidgetBase,
-			Chart2D, Theme, MoveSlice, Pie, ChartTooltip,
-			Bars, SummaryWidget){
+			domClass, domConstruct, on, xhr, Topic,
+			Chart2D, Theme, Pie,
+			SummaryWidget){
 
 	return declare([SummaryWidget], {
 		dataModel: "genome",
@@ -20,9 +16,11 @@ define([
 		baseQuery: "&eq(reference_genome,*)&select(reference_genome,genome_name,genome_id)&limit(25000)&facet((field,reference_genome),(mincount,1))&json(nl,map)",
 		columns: [
 			{label: "Type", field: "reference_genome"},
-			{label: "Genome Name", field: "genome_name", renderCell: function(obj, val, node){
+			{
+				label: "Genome Name", field: "genome_name", renderCell: function(obj, val, node){
 				node.innerHTML = '<a href="/view/Genome/' + obj.genome_id + '" target=_blank">' + val + '</a>';
-			}}
+			}
+			}
 		],
 		processData: function(res){
 
@@ -36,7 +34,10 @@ define([
 
 			var data = [];
 			Object.keys(d).forEach(function(key){
-				data.push({text: key + " (" + d[key] + ")", x: key, y: d[key]});
+				data.push({
+					text: key + " (" + d[key] + ")", x: key, y: d[key],
+					link: "#view_tab=genomes&filter=eq(reference_genome," + encodeURIComponent(key) + ")"
+				});
 			});
 
 			this.set('data', data);
@@ -64,15 +65,33 @@ define([
 					}
 				});
 
+				var onClickEventHandler = function(evt){
+					if(evt.type == "onclick" && evt.element == "slice"){
+						// console.log(evt);
+						var target = evt.run.data[evt.index].link;
+						if(target){
+							Topic.publish("/navigate", {href: window.location.pathname + target});
+						}
+					}
+					else if(evt.type == "onmouseover"){
+						var target = evt.run.data[evt.index].link;
+						if(target && !evt.eventMask.rawNode.style.cursor){
+							// console.log(evt.eventMask.rawNode);
+							evt.eventMask.rawNode.style.cursor = "pointer";
+						}
+					}
+				};
+
 				this.chart = new Chart2D(this.chartNode)
 					.setTheme(Theme)
 					.addPlot("default", {
 						type: this.DonutChart,
 						radius: 70,
-						stroke: "black",
 						label: true,
 						labelStyle: "columns"
 					});
+
+				this.chart.connectToPlot("default", onClickEventHandler);
 
 				this.chart.addSeries('x', this.data);
 				this.chart.render();
