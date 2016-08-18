95141eaa60ddf05a1b73d44283b4d597a00d7277
Dustin Machi <dmachi@dojotoolkit.org>
2016-08-17 23:58:47 -0400

    wip home page

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var getNews = require("../news");
 /* GET home page. */
 router.get('/', function(req, res){
 	req.applicationModule = "p3/app/p3app";
-	res.render('index', {title: 'PATRIC 3', request: req, response: res, news: getNews("&limit(4)&sort(-pubDate)")});
+	res.render('index', {title: 'PATRIC 3', request: req, response: res, news: getNews("&limit(3)&sort(-pubDate)")});
 });
 
 module.exports = router;
