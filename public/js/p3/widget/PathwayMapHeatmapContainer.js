790ae43cd5190a427c85814a0bc82c987768008d
Harry Yoo <hsyoo@anl.gov>
2016-08-31 16:42:01 -0500

    apply Topic.publish with target:"blank" for new window link

--- public/js/p3/widget/PathwayMapHeatmapContainer.js
+++ public/js/p3/widget/PathwayMapHeatmapContainer.js
@@ -320,7 +320,7 @@ define([
 				disabled: (featureIds.length === 0)
 			});
 			on(btnShowDetails.domNode, "click", function(){
-				window.open("/view/FeatureList/?in(feature_id,(" + featureIds + "))");
+				Topic.publish("/navigate", {href: "/view/FeatureList/?in(feature_id,(" + featureIds + "))", target: "blank"});
 			});
 
 			var btnAddToWorkspace = new Button({
