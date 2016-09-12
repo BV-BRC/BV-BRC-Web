12872f7a2dab41e4736534ad5e5d5b0e95165970
Harry Yoo <hsyoo@anl.gov>
2016-09-12 11:43:08 -0500

    rename "report problem" to "provide feedback"

--- public/js/p3/widget/ReportProblem.js
+++ public/js/p3/widget/ReportProblem.js
@@ -2,10 +2,10 @@ define([
 	"dojo/_base/declare", "dijit/_WidgetBase", "dojo/on",
 	"dojo/dom-class", "dijit/_TemplatedMixin", "dijit/_WidgetsInTemplateMixin",
 	"dojo/text!./templates/ReportProblem.html", "dijit/form/Form",
-	"dojo/topic","dojo/request","dojo/when"
+	"dojo/topic", "dojo/request", "dojo/when"
 ], function(declare, WidgetBase, on,
 			domClass, Templated, WidgetsInTemplate,
-			Template, FormMixin, Topic, request,when){
+			Template, FormMixin, Topic, request, when){
 	return declare([WidgetBase, FormMixin, Templated, WidgetsInTemplate], {
 		"baseClass": "CreateWorkspace",
 		templateString: Template,
@@ -29,23 +29,23 @@ define([
 				var values = this.getValues();
 				values.appVersion = window.App.appVersion;
 				values.appLabel = window.App.appLabel || "";
-				values.url = window.location.href;	
+				values.url = window.location.href;
 
-				if (window.App.user && window.App.user.id){
-					values.userId = window.App.user.id.replace("@patricbrc.org","");
+				if(window.App.user && window.App.user.id){
+					values.userId = window.App.user.id.replace("@patricbrc.org", "");
 				}
 
 				domClass.add(this.domNode, "Working");
-				console.log("Problem VALUES: ", values);
-			
+				// console.log("Problem VALUES: ", values);
+
 				when(request.post("/reportProblem", {
 					headers: {
 						"Authorization": (window.App.authorizationToken || "")
 					},
 					data: values
-				}),function(results){
-					console.log("Report Problem Results: ", results);
-                                        on.emit(_self.domNode, "dialogAction", {action: "close", bubbles: true})
+				}), function(results){
+					// console.log("Report Problem Results: ", results);
+					on.emit(_self.domNode, "dialogAction", {action: "close", bubbles: true})
 				}, function(err){
 					console.log("Error Reporting Problem: ", err);
 				});
@@ -55,7 +55,7 @@ define([
 		},
 
 		onCancel: function(evt){
-			console.log("Cancel/Close Dialog", evt);
+			// console.log("Cancel/Close Dialog", evt);
 			on.emit(this.domNode, "dialogAction", {action: "close", bubbles: true});
 		}
 	});
