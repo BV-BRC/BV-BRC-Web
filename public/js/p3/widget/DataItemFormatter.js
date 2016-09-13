a4281b0fc83a8151125bdde552f93e1ac690b819
Harry Yoo <hsyoo@anl.gov>
2016-09-13 11:22:27 -0500

    resolve #672

--- public/js/p3/widget/DataItemFormatter.js
+++ public/js/p3/widget/DataItemFormatter.js
@@ -809,6 +809,10 @@ define([
 				text: 'assembly_accession',
 				link: 'http://www.ncbi.nlm.nih.gov/assembly/'
 			}, {
+				name: "SRA Accession",
+				text: "sra_accession",
+				link: "http://www.ncbi.nlm.nih.gov/sra/?term="
+			}, {
 				name: 'GenBank Accessions',
 				text: 'genbank_accessions',
 				link: 'http://www.ncbi.nlm.nih.gov/nuccore/'
