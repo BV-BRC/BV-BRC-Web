966eb00822e4cafdde9ec9b7167f1e927ee566b2
Harry Yoo <hsyoo@anl.gov>
2016-10-03 13:50:41 -0500

    changed default value, code format

--- routes/reportProblem.js
+++ routes/reportProblem.js
@@ -1,84 +1,86 @@
 var express = require('express');
-var bodyParser= require("body-parser");
+var bodyParser = require("body-parser");
 var config = require("../config");
 var email = require("nodemailer");
 var smtpTransport = require("nodemailer-smtp-transport");
 var when = require("promised-io/promise").when;
 var defer = require("promised-io/promise").defer;
 
-function mail(message,subject,options){
-		console.log("Send Mail", message, subject);
-                if (!message){throw Error("Message is required for mail()");}
-                var u;
-                var transport;
-                var deferred = new defer();
-                var _self = this;
-                var mailconf = config.get("email");
-		var destMail = config.get("reportProblemEmailAddress");
+function mail(message, subject, options){
+	console.log("Send Mail", message, subject);
+	if(!message){
+		throw Error("Message is required for mail()");
+	}
 
+	var transport;
+	var deferred = new defer();
 
-		if (mailconf.localSendmail){
-			transport = email.createTransport();
-			//email.sendmail=true;
-                }else{
-//                      email.sendmail=false;
-                        email.SMTP = {
-                             host: mailconf.host || "localhost",
-                             port: mailconf.port || 25
-                        }
-                }
+	var mailconf = config.get("email");
+	var destMail = config.get("reportProblemEmailAddress");
 
-                if (mailconf.username) {
-                        email.SMTP.use_authentication=true;
-                        email.SMTP.user=mailconf.username;
-                        email.SMTP.pass=mailconf.password;
-                 }
+	if(mailconf.localSendmail){
+		transport = email.createTransport();
+		// email.sendmail=true;
+	}else{
+		// email.sendmail=false;
+		email.SMTP = {
+			host: mailconf.host || "localhost",
+			port: mailconf.port || 25
+		}
+	}
 
-                 if (!transport){
-                        var transportOpts = {
-                             host: mailconf.host || "localhost",
-                             port: mailconf.port || 25,
-                             debug: true,
-                         }
-                         if (mailconf.username){
-                             transportOpts.auth = {
-                                  user: mailconf.username,
-                                  pass: mailconf.password
-                              }
-                          }
-			  transportOpts.tls = { rejectUnauthorized: false }
-                          var transport = email.createTransport(smtpTransport(transportOpts));
-                  }
+	if(mailconf.username){
+		email.SMTP.use_authentication = true;
+		email.SMTP.user = mailconf.username;
+		email.SMTP.pass = mailconf.password;
+	}
 
-                  var mailmsg = {
-                          debug: true,
-                          to: destMail,
-                          sender: mailconf.defaultFrom, //"responder@hapticscience.com", // mailconf.defaultFrom,
-                          from: mailconf.defaultFrom,
-                          subject: subject || "No Subject",
-                          text: message
-                  }
+	if(!transport){
+		var transportOpts = {
+			host: mailconf.host || "localhost",
+			port: mailconf.port || 25,
+			debug: true,
+		}
+		if(mailconf.username){
+			transportOpts.auth = {
+				user: mailconf.username,
+				pass: mailconf.password
+			}
+		}
+		transportOpts.tls = {rejectUnauthorized: false}
+		transport = email.createTransport(smtpTransport(transportOpts));
+	}
 
-                 console.log("Sending Email: ",mailmsg);
+	var mailmsg = {
+		debug: true,
+		to: destMail,
+		sender: mailconf.defaultFrom, //"responder@hapticscience.com", // mailconf.defaultFrom,
+		from: mailconf.defaultFrom,
+		subject: subject || "No Subject",
+		text: message
+	}
 
+	console.log("Sending Email: ", mailmsg);
 
-                 transport.sendMail(mailmsg, function(err, result){
-                          console.log("sendMail result: ", err, result);
-                          if (deferred.fired){return;}
-                          if (err) {
-                               deferred.reject(err);
-                               return;
-                          }
+	transport.sendMail(mailmsg, function(err, result){
+		console.log("sendMail result: ", err, result);
+		if(deferred.fired){
+			return;
+		}
+		if(err){
+			deferred.reject(err);
+			return;
+		}
 
-                          deferred.resolve(result)
-                   });
+		deferred.resolve(result)
+	});
 
-                  return deferred.promise;
+	return deferred.promise;
 }
 
 function buildSubject(formBody){
-	var content=[]
-	if (formBody.appLabel){
+	var content = []
+	if(formBody.appLabel){
 		content.push("[" + formBody.appLabel + "]");
 	}
 	content.push(formBody.subject);
@@ -86,25 +88,25 @@ function buildSubject(formBody){
 	return content.join(" ");
 }
 function buildMessage(formBody){
-	var content=[]
+	var content = []
 
-	content.push("Version: " + formBody.appVersion + " " + (formBody.appLabel||""));
-	content.push("URL: "+ formBody.url);
-	content.push("User ID: "+ formBody.userId);
+	content.push("Version: " + formBody.appVersion + " " + (formBody.appLabel || ""));
+	content.push("URL: " + formBody.url);
+	content.push("User ID: " + formBody.userId);
 	content.push("\n" + formBody.content);
 	return content.join("\n");
 }
 module.exports = [
-	bodyParser.urlencoded(),
-	function(req,res,next){
+	bodyParser.urlencoded({extended: true}),
+	function(req, res, next){
 		console.log("Report A Problem: ", req.body);
 		var message = buildMessage(req.body);
-		var subject = buildSubject(req.body); 
+		var subject = buildSubject(req.body);
 
 		console.log("Report Message: ", message);
 		console.log("Report Subject: ", subject);
 
-		when(mail(message,subject), function(){	
+		when(mail(message, subject), function(){
 			console.log("Problem Report Mail Sent");
 			res.status(201).end();
 		}, function(err){
