16eb8b6eeda3e5189537247ddb9e09b5718ab8f9
Dustin Machi <dmachi@dojotoolkit.org>
2016-10-06 13:43:22 -0400

    release js, bump version

--- public/js/release/p3/widget/AdvancedSearch.js
+++ public/js/release/p3/widget/AdvancedSearch.js
@@ -171,7 +171,7 @@ define("p3/widget/AdvancedSearch", [
 		},
 
 		formatgenome_feature: function(docs,total){
-			var out=["<div class=\"searchResultsContainer featureResults\">",'<div class="resultTypeHeader"><a href="/view/FeatureList/?',this.state.search,"#view_tab=overview",'">Genome Features&nbsp;(', total, ")</div> </a>"];
+			var out=["<div class=\"searchResultsContainer featureResults\">",'<div class="resultTypeHeader"><a href="/view/FeatureList/?',this.state.search,"#view_tab=features",'">Genome Features&nbsp;(', total, ")</div> </a>"];
 			docs.forEach(function(doc){
 				out.push("<div class='searchResult'>");
 				out.push("<div class='resultHead'><a href='/view/Feature/" + doc.feature_id + "'>" + (doc.product || doc.patric_id || doc.refseq_locus_tag || doc.alt_locus_tag) + "</a>");
@@ -359,7 +359,7 @@ define("p3/widget/AdvancedSearch", [
 						break;
 				}
 
-				q[type] = {dataType: type, accept: "application/solr+json", query: tq + "&limit(3)&sort(+score)" }
+				q[type] = {dataType: type, accept: "application/solr+json", query: tq + "&limit(3)&sort(-score)" }
 			})
 
 			console.log("SEARCH: ", q);
