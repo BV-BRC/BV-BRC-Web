d4f63f591f7fa215bb7bf937b1f232662d2962a5
Harry Yoo <hsyoo@anl.gov>
2016-10-13 11:29:46 -0500

    resolve PATRIC3/patric3_webiste#1077

--- public/js/p3/widget/formatter.js
+++ public/js/p3/widget/formatter.js
@@ -43,7 +43,166 @@ define(["dojo/date/locale", "dojo/dom-construct", "dojo/dom-class"], function(lo
 		return locale.format(obj, format || {formatLength: "short"});
 	};
 
+	var getExternalLinks = function(target){
+		var link;
+
+		if (target.match(/ncbi_gene/i)) {
+			link = "//www.ncbi.nlm.nih.gov/sites/entrez?db=gene&cmd=Retrieve&dopt=full_report&list_uids=";
+		}
+		else if (target.match(/ncbi_accession/i)) {
+			link = "//www.ncbi.nlm.nih.gov/entrez/viewer.fcgi?db=nucleotide&val=";
+		}
+		else if (target.match(/ncbi_protein/i) || target.match(/RefSeq/i) || target.match(/GI/i)) {
+			link = "//www.ncbi.nlm.nih.gov/protein/";
+		}
+		else if (target.match(/RefSeq_NT/i)) {
+			link = "//www.ncbi.nlm.nih.gov/nuccore/"; // NC_010067.1 - // nucleotide db
+		}
+		else if (target.match(/go_term/i)) {
+			link = "http://amigo.geneontology.org/cgi-bin/amigo/term_details?term="; // GO:0004747
+		}
+		else if (target.match(/ec_number/i)) {
+			link = "http://enzyme.expasy.org/EC/"; // 2.7.1.15
+		}
+		else if (target.match(/kegg_pathwaymap/i) || target.match(/KEGG/i)) {
+			link = "http://www.genome.jp/dbget-bin/www_bget?"; // pathway+map00010
+		}
+		else if (target.match(/UniProtKB-Accession/i) || target.match(/UniProtKB-ID/i)) {
+			link = "http://www.uniprot.org/uniprot/"; // A9MFG0 or ASTD_SALAR
+		}
+		else if (target.match(/UniRef100/i) || target.match(/UniRef90/i) || target.match(/UniRef50/i)) {
+			link = "http://www.uniprot.org/uniref/"; // UniRef100_A9MFG0, UniRef90_B5F7J0, or // UniRef50_Q1C8A9
+		}
+		else if (target.match(/UniParc/i)) {
+			link = "http://www.uniprot.org/uniparc/"; // UPI0001603B3F
+		}
+		else if (target.match(/EMBL/i) || target.match(/EMBL-CDS/i)) {
+			link = "//www.ebi.ac.uk/ena/data/view/"; // CP000880, ABX21565
+		}
+		else if (target.match(/GeneID/i)) {
+			link = "//www.ncbi.nlm.nih.gov/sites/entrez?db=gene&term="; // 5763416;
+		}
+		else if (target.match(/GenomeReviews/i)) {
+			link = "http://www.genomereviews.ebi.ac.uk/GR/contigview?chr="; // CP000880_GR
+		}
+		else if (target.match(/eggNOG/i)) {
+			link = "http://eggnog.embl.de/cgi_bin/display_multi_clusters.pl?linksource=uniprot&level=0&1="; // Q2YII1 -- uniprot accession
+		}
+		else if (target.match(/HOGENOM/i)) {
+			link = "http://pbil.univ-lyon1.fr/cgi-bin/acnuc-ac2tree?db=HOGENOM&query="; // A9MFG0 -- uniprot accession
+		}
+		else if (target.match(/OMA/i)) {
+			link = "http://omabrowser.org/cgi-bin/gateway.pl?f=DisplayGroup&p1="; // A9MFG0 -- uniprot accession
+		}
+		else if (target.match(/ProtClustDB/i)) {
+			link = "//www.ncbi.nlm.nih.gov/sites/entrez?Db=proteinclusters&Cmd=DetailsSearch&Term="; // A9MFG0 -- uniprot accession
+		}
+		else if (target.match(/BioCyc/i)) {
+			link = "http://biocyc.org/getid?id="; // BMEL359391:BAB2_0179-MONOMER
+		}
+		else if (target.match(/NMPDR/i)) {
+			link = "//www.nmpdr.org/linkin.cgi?id="; // fig|382638.8.peg.1669"
+		}
+		else if (target.match(/EnsemblGenome/i) || target.match(/EnsemblGenome_TRS/i)
+			|| target.match(/EnsemblGenome_PRO/i)) {
+			link = "http://www.ensemblgenomes.org/id/"; // EBMYCT00000005579
+		}
+		else if (target.match(/BEIR/i)) {
+			link = "http://www.beiresources.org/Catalog/ItemDetails/tabid/522/Default.aspx?Template=Clones&BEINum=";
+		}
+		else if (target.match(/PDB/i)) {
+			link = "Jmol?structureID=";
+		}
+		else if (target.match(/STRING/i)) { // 204722.BR0001
+			link = "http://string.embl.de/newstring_cgi/show_network_section.pl?identifier=";
+		}
+		else if (target.match(/MEROPS/i)) { // M50.005
+			link = "http://merops.sanger.ac.uk/cgi-bin/pepsum?id=";
+		}
+		else if (target.match(/PATRIC/i)) { // 17788255
+			link = "Feature?cType=feature&cId=";
+		}
+		else if (target.match(/OrthoDB/i)) { // EOG689HR1
+			link = "http://cegg.unige.ch/orthodb7/results?searchtext=";
+		}
+		else if (target.match(/NCBI_TaxID/i)) { // 29461
+			link = "//www.ncbi.nlm.nih.gov/Taxonomy/Browser/wwwtax.cgi?mode=Info&id=";
+		}
+		else if (target.match(/KO/i)) { // K04756
+			link = "http://www.genome.jp/dbget-bin/www_bget?ko:";
+		}
+		else if (target.match(/TubercuList/i)) { // Rv2429
+			link = "http://tuberculist.epfl.ch/quicksearch.php?gene+name=";
+		}
+		else if (target.match(/PeroxiBase/i)) { // 4558
+			link = "http://peroxibase.toulouse.inra.fr/browse/process/view_perox.php?id=";
+		}
+		else if (target.match(/Reactome/i)) { // REACT_116125
+			link = "http://www.reactome.org/cgi-bin/eventbrowser_st_id?ST_ID=";
+		}
+		else if (target.match(/VFDB/i)) {
+			link = "http://www.mgc.ac.cn/cgi-bin/VFs/gene.cgi?GeneID="; // VFG1817
+		}
+		else if (target.match(/VFDB_HOME/i)) {
+			link = "http://www.mgc.ac.cn/VFs/";
+		}
+		else if (target.match(/Victors/i)) {
+			link = "http://www.phidias.us/victors/gene_detail.php?c_mc_victor_id="; // 220
+		}
+		else if (target.match(/Victors_HOME/i)) {
+			link = "http://www.phidias.us/victors/";
+		}
+		else if (target.match(/PATRIC_VF/i)) {
+			link = "SpecialtyGeneEvidence?source=PATRIC_VF&sourceId="; // Rv3875
+		}
+		else if (target.match(/PATRIC_VF_HOME/i)) {
+			link = "SpecialtyGeneSource?source=PATRIC_VF&kw=";
+		}
+		else if (target.match(/ARDB/i)) {
+			link = "//ardb.cbcb.umd.edu/cgi/search.cgi?db=R&term="; // AAL09826
+		}
+		else if (target.match(/ARDB_HOME/i)) {
+			link = "//ardb.cbcb.umd.edu/";
+		}
+		else if (target.match(/CARD/i)) {
+			link = ""; //TODO: need to add
+		}
+		else if (target.match(/CARD_HOME/i)) {
+			link = "http://arpcard.mcmaster.ca";
+		}
+		else if (target.match(/DrugBank/i)) {
+			link = "http://v3.drugbank.ca/molecules/"; // 1
+		}
+		else if (target.match(/DrugBank_HOME/i)) {
+			link = "http://v3.drugbank.ca";
+		}
+		else if (target.match(/TTD/i)) {
+			link = "http://bidd.nus.edu.sg/group/TTD/ZFTTDDetail.asp?ID="; // TTDS00427
+		}
+		else if (target.match(/TTD_HOME/i)) {
+			link = "http://bidd.nus.edu.sg/group/TTD/ttd.asp";
+		}
+		else if (target.match(/Human/i)) {
+			link = "//www.ncbi.nlm.nih.gov/protein/"; // NP_001005484.1
+		}
+		else if (target.match(/Human_HOME/i)) {
+			link = "//www.ncbi.nlm.nih.gov/assembly/GCF_000001405.26";
+		}
+		else if (target.match(/bioproject_accession/i)) {
+			link = "http://www.ncbi.nlm.nih.gov/bioproject/?term=";
+		}
+		else if (target.match(/biosample_accession/i)) {
+			link = "http://www.ncbi.nlm.nih.gov/biosample/";
+		}
+		else if (target.match(/assembly_accession/i)) {
+			link = "http://www.ncbi.nlm.nih.gov/assembly/";
+		}
+		// edit patric-searches-and-tools/WebContent/js/specialty_gene_list_grids.js as well
+		return link;
+	}
+
 	var formatters = {
+		getExternalLinks: getExternalLinks,
 		dateOnly: function(obj){
 			return dateFormatter(obj, {selector: "date", formatLength: "short"});
 		},
