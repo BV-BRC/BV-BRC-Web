6f57a70b7d03b1164fd8bd6da34f8f19f10c3838
Harry Yoo <hsyoo@anl.gov>
2016-11-21 15:35:45 -0600

    override action buttons to open in a new tab

--- public/js/p3/widget/BlastResultGridContainer.js
+++ public/js/p3/widget/BlastResultGridContainer.js
@@ -1,10 +1,12 @@
 define([
 	"dojo/_base/declare", "dojo/_base/lang",
 	"dojo/topic",
-	"./GridContainer", "./BlastResultGrid"
+	"dijit/popup",
+	"./GridContainer", "./BlastResultGrid", "./PerspectiveToolTip"
 ], function(declare, lang,
 			Topic,
-			GridContainer, BlastResultGrid){
+			popup,
+			GridContainer, BlastResultGrid, PerspectiveToolTipDialog){
 
 	return declare([GridContainer], {
 		gridCtor: BlastResultGrid,
@@ -34,6 +36,10 @@ define([
 				this.grid.set('state', state);
 			}
 		},
+		/*
+		add GenomeBrowser as a special case and override ViewGenomeItem, ViewGenomeItems, ViewCDSFeaturesSeq,
+		ViewFeatureItem, ViewFeatureItem to open in a new tab
+		*/
 		selectionActions: GridContainer.prototype.selectionActions.concat([
 			[
 				"GenomeBrowser",
@@ -55,6 +61,177 @@ define([
 
 					Topic.publish("/navigate", {
 						href: "/view/Genome/" + target.genome_id + hash,
+						target: "blank"
+					});
+				},
+				false
+			],
+			[
+				"ViewGenomeItem",
+				"MultiButton fa icon-selection-Genome fa-2x",
+				{
+					label: "GENOME",
+					validTypes: ["*"],
+					multiple: false,
+					tooltip: "Switch to Genome View. Press and Hold for more options.",
+					ignoreDataType: true,
+					validContainerTypes: ["sequence_data", "feature_data", "spgene_data", "sequence_data"],
+					pressAndHold: function(selection, button, opts, evt){
+						popup.open({
+							popup: new PerspectiveToolTipDialog({perspectiveUrl: "/view/Genome/" + selection[0].genome_id}),
+							around: button,
+							orient: ["below"]
+						});
+
+					}
+				},
+				function(selection){
+					var sel = selection[0];
+
+					Topic.publish("/navigate", {href: "/view/Genome/" + sel.genome_id, target: "blank"});
+				},
+				false
+			],
+			[
+				"ViewGenomeItems",
+				"MultiButton fa icon-selection-GenomeList fa-2x",
+				{
+					label: "GENOMES",
+					validTypes: ["*"],
+					multiple: true,
+					min: 2,
+					max: 1000,
+					tooltip: "Switch to Genome List View. Press and Hold for more options.",
+					ignoreDataType: true,
+					validContainerTypes: ["genome_data", "sequence_data", "feature_data", "spgene_data", "sequence_data"],
+					pressAndHold: function(selection, button, opts, evt){
+						var map = {};
+						selection.forEach(function(sel){
+							if(!map[sel.genome_id]){
+								map[sel.genome_id] = true
+							}
+						})
+						var genome_ids = Object.keys(map);
+						popup.open({
+							popup: new PerspectiveToolTipDialog({
+								perspective: "GenomeList",
+								perspectiveUrl: "/view/GenomeList/?in(genome_id,(" + genome_ids.join(",") + "))"
+							}),
+							around: button,
+							orient: ["below"]
+						});
+
+					}
+				},
+				function(selection){
+					var map = {};
+					selection.forEach(function(sel){
+						if(!map[sel.genome_id]){
+							map[sel.genome_id] = true
+						}
+					});
+					var genome_ids = Object.keys(map);
+					Topic.publish("/navigate", {
+						href: "/view/GenomeList/?in(genome_id,(" + genome_ids.join(",") + "))",
+						target: "blank"
+					});
+				},
+				false
+			],
+			[
+				"ViewCDSFeaturesSeq",
+				"MultiButton fa icon-selection-FeatureList fa-2x",
+				{
+					label: "FEATURES",
+					validTypes: ["*"],
+					multiple: false,
+					tooltip: "Switch to Feature List View. Press and Hold for more options.",
+					validContainerTypes: ["sequence_data"],
+					pressAndHold: function(selection, button, opts, evt){
+						// console.log("PressAndHold");
+						// console.log("Selection: ", selection, selection[0])
+						popup.open({
+							popup: new PerspectiveToolTipDialog({
+								perspective: "FeatureList",
+								perspectiveUrl: "/view/FeatureList/?and(eq(annotation,PATRIC),eq(feature_type,CDS),eq(sequence_id," + selection[0].sequence_id + "))"
+							}),
+							around: button,
+							orient: ["below"]
+						});
+					}
+				},
+				function(selection){
+
+					var sel = selection[0];
+					Topic.publish("/navigate", {
+						href: "/view/FeatureList/?and(eq(annotation,PATRIC),eq(sequence_id," + sel.sequence_id + "),eq(feature_type,CDS))",
+						target: "blank"
+					});
+				},
+				false
+			],
+			[
+				"ViewFeatureItem",
+				"MultiButton fa icon-selection-Feature fa-2x",
+				{
+					label: "FEATURE",
+					validTypes: ["*"],
+					multiple: false,
+					tooltip: "Switch to Feature View. Press and Hold for more options.",
+					validContainerTypes: ["feature_data"],
+					pressAndHold: function(selection, button, opts, evt){
+
+						popup.open({
+							popup: new PerspectiveToolTipDialog({
+								perspective: "Feature",
+								perspectiveUrl: "/view/Feature/" + selection[0].feature_id
+							}),
+							around: button,
+							orient: ["below"]
+						});
+					}
+				},
+				function(selection){
+					var sel = selection[0];
+					Topic.publish("/navigate", {
+						href: "/view/Feature/" + sel.feature_id + "#view_tab=overview",
+						target: "blank"
+					});
+				},
+				false
+			],
+			[
+				"ViewFeatureItems",
+				"MultiButton fa icon-selection-FeatureList fa-2x",
+				{
+					label: "FEATURES",
+					validTypes: ["*"],
+					multiple: true,
+					min: 2,
+					max: 5000,
+					tooltip: "Switch to Feature List View. Press and Hold for more options.",
+					validContainerTypes: ["feature_data"],
+					pressAndHold: function(selection, button, opts, evt){
+
+						popup.open({
+							popup: new PerspectiveToolTipDialog({
+								perspective: "FeatureList",
+								perspectiveUrl: "/view/FeatureList/?in(feature_id,(" + selection.map(function(x){
+									return x.feature_id;
+								}).join(",") + "))"
+							}),
+							around: button,
+							orient: ["below"]
+						});
+
+					}
+				},
+				function(selection){
+
+					Topic.publish("/navigate", {
+						href: "/view/FeatureList/?in(feature_id,(" + selection.map(function(x){
+							return x.feature_id;
+						}).join(",") + "))",
 						target: "blank"
 					});
 				},
