0152cc4a59dedf1bdd85d82e724e5ceb10db7b88
Harry Yoo <hsyoo@anl.gov>
2017-03-01 10:41:24 -0600

    bump verstion to 3.2.85

--- public/js/release/p3/widget/app/GenomeDistance.js
+++ public/js/release/p3/widget/app/GenomeDistance.js
@@ -1,5 +1,5 @@
 require({cache:{
-'url:p3/widget/app/templates/GenomeDistance.html':"<form dojoAttachPoint=\"containerNode\" class=\"PanelForm\"\n      dojoAttachEvent=\"onreset:_onReset,onsubmit:_onSubmit,onchange:validate\">\n    <div class=\"appTemplate\">\n        <div class=\"appTitle\">\n            <span class=\"breadcrumb\">Services</span>\n            <h3>Similar Genome Finder</h3>\n            <p>Find similar genomes or compute genome distance estimation using Mash/MinHash.</p>\n        </div>\n\n        <div class=\"service_form\">\n            <div class=\"appBox appShadow\">\n                <div class=\"headerrow\"  style=\"margin-bottom: 7px\">\n                    <label class=\"appBoxLabel\">Select a Genome</label>\n                </div>\n                <div data-dojo-attach-event=\"onChange:onSuggestNameChange\"\n                     data-dojo-type=\"p3/widget/GenomeNameSelector\"\n                     data-dojo-attach-point=\"genome_id\"\n                     data-dojo-props=\"placeHolder:'e.g. M. tuberculosis CDC1551'\"\n                     style=\"width:600px;\">\n                </div>\n                <div class=\"form_msg_warning\" data-dojo-attach-point=\"genome_message\"></div>\n            </div>\n\n            <div class=\"appBox appShadow hidden\" data-dojo-attach-point=\"fasta_wrapper\">\n                <div class=\"headerrow\" style=\"margin-bottom: 7px\">\n                    <label class=\"appBoxLabel\">Upload FASTA</label>\n                </div>\n                <div name=\"fasta\" style=\"width:100%\"></div>\n\n                <div class=\"form_msg_warning\" data-dojo-attach-point=\"fasta_message\"></div>\n            </div>\n\n            <div class=\"appRow\" style=\"text-align:center\">\n                <div class=\"appRowSegment\" data-dojo-attach-point=\"advanced\">\n                    <label class=\"largelabel\">Advanced options</label>\n                    <div class=\"iconbox\" style=\"margin-left:0\">\n                        <i data-dojo-attach-point=\"advancedOptionIcon\" class=\"fa icon-caret-down fa-1\"></i>\n                    </div>\n                </div>\n            </div>\n\n            <div data-dojo-attach-point=\"advancedOptions\" style=\"display: none\">\n                <div class=\"appBox appShadow\">\n                    <div class=\"headerrow\">\n                        <label class=\"appBoxLabel\">Parameters</label>\n                    </div>\n                    <div class=\"left quarter\">\n                        <label class=\"paramlabel\">Max hits: </label><br/>\n                        <select name=\"max_target\" style=\"width: 100px\"\n                                data-dojo-type=\"dijit/form/Select\"\n                                data-dojo-attach-point=\"max_hits\">\n                            <option value=\"1\">1</option>\n                            <option value=\"10\">10</option>\n                            <option value=\"50\" selected=\"selected\">50</option>\n                            <option value=\"100\">100</option>\n                            <option value=\"500\">500</option>\n                        </select>\n                    </div>\n\n                    <div class=\"left quarter\">\n                        <label class=\"paramlabel\">P value threshold: </label><br/>\n                        <div data-dojo-type=\"dijit/form/Select\"\n                             data-dojo-attach-point=\"pvalue\"\n                             value=\"0.01\" style=\"width:60px\">\n                            <option value=\"0.001\">0.001</option>\n                            <option value=\"0.01\">0.01</option>\n                            <option value=\"0.1\">0.1</option>\n                            <option value=\"1\">1</option>\n                            <option value=\"10\">10</option>\n                        </div>\n                    </div>\n                    <div class=\"left quarter\">\n                        <label class=\"paramlabel\">Distance: </label><br/>\n                        <div data-dojo-type=\"dijit/form/Select\"\n                             data-dojo-attach-point=\"distance\"\n                             value=\"0.01\" style=\"width:60px\">\n                            <option value=\"0.001\">0.001</option>\n                            <option value=\"0.05\">0.05</option>\n                            <option value=\"0.01\">0.01</option>\n                            <option value=\"0.05\">0.05</option>\n                            <option value=\"0.1\">0.1</option>\n                            <option value=\"0.5\">0.5</option>\n                            <option value=\"1\">1</option>\n                        </div>\n                    </div>\n\n                    <div class=\"left quarter\">\n                        <div data-dojo-type=\"dijit/form/CheckBox\"\n                             data-dojo-attach-point=\"include_reference\"\n                             value=\"1\"></div>\n                        <label class=\"paramlabel\">Limit to reference</label>\n                        <br/>\n                        <div data-dojo-type=\"dijit/form/CheckBox\"\n                            data-dojo-attach-point=\"include_representative\"\n                             value=\"1\"></div>\n                        <label class=\"paramlabel\">Limit to representative</label>\n\n                    </div>\n                    <div class=\"clear\"></div>\n                </div>\n            </div>\n\n        </div>\n        <div class=\"appSubmissionArea\">\n            <div style=\"margin-top: 10px; text-align:center;\">\n                <div data-dojo-type=\"dijit/form/Button\"\n                     data-dojo-attach-point=\"mapButton\"\n                     data-dojo-attach-event=\"onClick:submit\"\n                     data-dojo-props=\"disabled:false\">\n                    Search\n                </div>\n            </div>\n        </div>\n        <div class=\"reSubmitBtn\" style=\"visibility: hidden\"\n             data-dojo-type=\"dijit/form/Button\"\n             data-dojo-attach-event=\"onClick:resubmit\">Edit form and resubmit\n        </div>\n    </div>\n\n    <div class=\"service_error hidden\">\n        <h3></h3>\n        <div class=\"service_message\"></div>\n    </div>\n    <div class=\"genomedistance_result\"></div>\n</form>\n\n"}});
+'url:p3/widget/app/templates/GenomeDistance.html':"<form dojoAttachPoint=\"containerNode\" class=\"PanelForm\"\n      dojoAttachEvent=\"onreset:_onReset,onsubmit:_onSubmit,onchange:validate\">\n    <div class=\"appTemplate\">\n        <div class=\"appTitle\">\n            <span class=\"breadcrumb\">Services</span>\n            <h3>Similar Genome Finder</h3>\n            <p>Find similar public genomes or compute genome distance estimation using Mash/MinHash.</p>\n        </div>\n\n        <div class=\"service_form\">\n            <div class=\"appBox appShadow\">\n                <div class=\"headerrow\" style=\"margin-bottom: 7px\">\n                    <label class=\"appBoxLabel\">Select a genome</label>\n                </div>\n                <div class=\"appRow\">\n                    <div class=\"paramlabel\">Search by genome name or genome ID</div>\n                    <div data-dojo-attach-event=\"onChange:onSuggestNameChange\"\n                         data-dojo-type=\"p3/widget/GenomeNameSelector\"\n                         data-dojo-attach-point=\"genome_id\"\n                         data-dojo-props=\"placeHolder:'e.g. M. tuberculosis CDC1551'\"\n                         style=\"width:600px;\">\n                    </div>\n                    <div class=\"form_msg_warning\" data-dojo-attach-point=\"genome_message\"></div>\n                </div>\n\n                <div class=\"appRow\" data-dojo-attach-point=\"fasta_wrapper\">\n                    <label class=\"paramlabel\">OR Upload FASTA</label>\n                    <div name=\"fasta\" style=\"width:100%\"></div>\n\n                    <div class=\"form_msg_warning\" data-dojo-attach-point=\"fasta_message\"></div>\n                </div>\n            </div>\n\n            <div class=\"appRow\" style=\"text-align:center\">\n                <div class=\"appRowSegment\" data-dojo-attach-point=\"advanced\">\n                    <label class=\"largelabel\">Advanced options</label>\n                    <div class=\"iconbox\" style=\"margin-left:0\">\n                        <i data-dojo-attach-point=\"advancedOptionIcon\" class=\"fa icon-caret-down fa-1\"></i>\n                    </div>\n                </div>\n            </div>\n\n            <div data-dojo-attach-point=\"advancedOptions\" style=\"display: none\">\n                <div class=\"appBox appShadow\">\n                    <div class=\"headerrow\">\n                        <label class=\"appBoxLabel\">Parameters</label>\n                    </div>\n                    <div class=\"left\" style=\"width:20%\">\n                        <label class=\"paramlabel\">Max hits: </label><br/>\n                        <select name=\"max_target\" style=\"width: 100px\"\n                                data-dojo-type=\"dijit/form/Select\"\n                                data-dojo-attach-point=\"max_hits\">\n                            <option value=\"1\">1</option>\n                            <option value=\"10\">10</option>\n                            <option value=\"50\" selected=\"selected\">50</option>\n                            <option value=\"100\">100</option>\n                            <option value=\"500\">500</option>\n                        </select>\n                    </div>\n\n                    <div class=\"left\" style=\"width:20%\">\n                        <label class=\"paramlabel\">P value threshold: </label><br/>\n                        <div data-dojo-type=\"dijit/form/Select\"\n                             data-dojo-attach-point=\"pvalue\"\n                             value=\"1\" style=\"width:60px\">\n                            <option value=\"0.001\">0.001</option>\n                            <option value=\"0.01\">0.01</option>\n                            <option value=\"0.1\">0.1</option>\n                            <option value=\"1\">1</option>\n                        </div>\n                    </div>\n                    <div class=\"left\" style=\"width:20%\">\n                        <label class=\"paramlabel\">Distance: </label><br/>\n                        <div data-dojo-type=\"dijit/form/Select\"\n                             data-dojo-attach-point=\"distance\"\n                             value=\"0.05\" style=\"width:60px\">\n                            <option value=\"0.01\">0.01</option>\n                            <option value=\"0.05\">0.05</option>\n                            <option value=\"0.1\">0.1</option>\n                            <option value=\"0.5\">0.5</option>\n                            <option value=\"1\">1</option>\n                        </div>\n                    </div>\n\n                    <div class=\"left\">\n                        <label class=\"paramlabel\">Scope:</label><br/>\n                        <div data-dojo-type=\"dijit/form/RadioButton\"\n                             data-dojo-attach-point=\"scope_ref\"\n                             name=\"scope\"\n                             checked=\"checked\"\n                             value=\"ref\"></div>\n                        <label>Reference and representative genomes</label>\n                        <br/>\n                        <div data-dojo-type=\"dijit/form/RadioButton\"\n                             data-dojo-attach-point=\"scope_all\"\n                             name=\"scope\"\n                             value=\"all\"></div>\n                        <label>All public genomes</label>\n\n                    </div>\n                    <div class=\"clear\"></div>\n                </div>\n            </div>\n\n        </div>\n        <div class=\"appSubmissionArea\">\n            <div style=\"margin-top: 10px; text-align:center;\">\n                <div data-dojo-type=\"dijit/form/Button\"\n                     data-dojo-attach-point=\"mapButton\"\n                     data-dojo-attach-event=\"onClick:submit\"\n                     data-dojo-props=\"disabled:false\">\n                    Search\n                </div>\n            </div>\n        </div>\n        <div class=\"reSubmitBtn\" style=\"visibility: hidden\"\n             data-dojo-type=\"dijit/form/Button\"\n             data-dojo-attach-event=\"onClick:resubmit\">Edit form and resubmit\n        </div>\n    </div>\n\n    <div class=\"service_error hidden\">\n        <h3></h3>\n        <div class=\"service_message\"></div>\n    </div>\n    <div class=\"genomedistance_result\"></div>\n</form>\n\n"}});
 define("p3/widget/app/GenomeDistance", [
 	"dojo/_base/declare", "dojo/_base/lang", "dojo/_base/Deferred",
 	"dojo/on", "dojo/query", "dojo/dom-class", "dojo/dom-construct", "dojo/dom-style", "dojo/topic",
@@ -32,6 +32,8 @@ define("p3/widget/app/GenomeDistance", [
 				// console.log("default path: ", this.defaultPath);
 				this.fasta.set('path', this.defaultPath);
 				this.fasta.set('type', ['contigs']);
+				this.fasta.set('required', false);
+				this.fasta.on('change', lang.hitch(this, "onFastaChange"));
 				this.fasta.placeAt(fastaDom, "only");
 
 				domClass.remove(this.fasta_wrapper, "hidden");
@@ -78,22 +80,9 @@ define("p3/widget/app/GenomeDistance", [
 		},
 
 		validate: function(){
-			// console.log("validate", this.sequence.get('value'), (this.sequence.get('value')).length,  this.database.get('value'), this.program.get('value'));
+			// console.log("validate", this.genome_id.get('value'), this.fasta.get('value'), !(this.genome_id.get('value') == '' && this.fasta.get('value') == ''));
 
-			// var sequence = this.sequence.get('value');
-			//
-			// if(sequence && sequence.length > 1
-			// 	&& this.database.get('value')
-			// 	&& this.program.get('value')
-			// 	&& this.hasSingleFastaSequence(sequence)){
-			//
-			// 	// console.log("validation passed");
-			// 	this.mapButton.set('disabled', false);
-				return true;
-			// }else{
-			// 	this.mapButton.set('disabled', true);
-			// 	return false;
-			// }
+			return !(this.genome_id.get('value') == '' && this.fasta.get('value') == '');
 		},
 
 		submit: function(){
@@ -105,8 +94,16 @@ define("p3/widget/app/GenomeDistance", [
 			var max_pvalue = parseFloat(this.pvalue.get('value')) || 0.01;
 			var max_distance = parseFloat(this.distance.get('value')) || 0.01;
 			var max_hits = parseInt(this.max_hits.get('value'));
-			var include_reference = this.include_reference.get('value') ? 1 : 0;
-			var include_representative = this.include_representative.get('value') ? 1 : 0;
+			var scope_ref = this.scope_ref.get('value');
+			var scope_all = this.scope_all.get('value');
+			var include_reference, include_representative;
+			if(scope_ref == "ref" && scope_all === false){
+				include_reference = 1;
+				include_representative = 1;
+			}else{
+				include_reference = 0;
+				include_representative = 0;
+			}
 
 			var def = new Deferred();
 			var resultType = "genome";
@@ -173,7 +170,7 @@ define("p3/widget/app/GenomeDistance", [
 
 		showNoResultMessage: function(){
 			domClass.remove(query(".service_error")[0], "hidden");
-			query(".service_error h3")[0].innerHTML = "Genome Distance Service has no match. Please revise query and submit again.";
+			query(".service_error h3")[0].innerHTML = "Similar Genome Finder returned no hit. Please revise parameters and submit again.";
 			domClass.add(query(".service_message")[0], "hidden");
 
 			query(".genomedistance_result .GridContainer").style("visibility", "hidden");
@@ -183,7 +180,24 @@ define("p3/widget/app/GenomeDistance", [
 			domStyle.set(this.result.domNode, 'visibility', 'hidden');
 		},
 
+		onFastaChange: function(){
+			// when fasta file chosen
+			// console.log("onFastaChange", this.fasta.get('value'), this.genome_id.get('value'));
+			if(this.genome_id.get('value') !== '' && this.fasta.get('value') !== ''){
+				this.genome_id.reset();
+			}
+			// 2nd call due to the genome name change
+			// if(this.genome_id.get('value') !== '' && this.fasta.get('value') == ''){
+			// 	console.log("2nd call");
+			// }
+			this.validate();
+		},
+
 		onSuggestNameChange: function(){
+			// console.log("onGenomeIDChange", this.fasta.get('value'), this.genome_id.get('value'));
+			if(this.fasta && this.fasta.get('value') !== '' && this.genome_id.get('value') !== ''){
+				this.fasta.reset();
+			}
 			this.validate();
 		}
 	});
