b937d46dae5e12ebee2a95de3aa6d6fdecdc8f42
Harry Yoo <hsyoo@anl.gov>
2017-03-06 12:44:56 -0600

    handle amr tab

--- public/js/p3/widget/viewer/Taxonomy.js
+++ public/js/p3/widget/viewer/Taxonomy.js
@@ -202,6 +202,14 @@ define([
 				case "genomes":
 					activeTab.set("state", lang.mixin({}, this.state));
 					break;
+				case "amr":
+					// need to show all (prevent referenceGenomes)
+					if(this.state.genome_ids){
+						activeTab.set("state", lang.mixin({}, this.state, {
+							search: "in(genome_id,(" + this.state.genome_ids.join(",") + "))"
+						}))
+					}
+					break;
 				case "interactions":
 					// console.log("interactions tab", this.setActivePanelState.caller, this.state);
 					if(this.state.genome_ids){
