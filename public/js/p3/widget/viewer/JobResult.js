ed022a889e624b2bec7f7a88f9c6b4920e0bffb0
Zane Reynolds <zane.reynolds@gmail.com>
2017-09-28 12:02:51 -0400

    a little cleanup

--- public/js/p3/widget/viewer/JobResult.js
+++ public/js/p3/widget/viewer/JobResult.js
@@ -108,13 +108,7 @@ define([
 						var tableLabel = this._jobOut[prop].hasOwnProperty("label") ? this._jobOut[prop]["label"] : prop;
 						var tableValue = this._jobOut[prop].hasOwnProperty("format") ? this._jobOut[prop]["format"](this.data.autoMeta[prop]) : this.data.autoMeta[prop];
 						if(prop == 'parameters') {
-							// this.params = new TitlePane({
-							// 	title: "Parameters",
-							// 	style: "margin-bottom:5px;",
-							// 	open: false,
-							// 	content: tableValue
-							// });
-							job_output.push('<tr class="alt"><td class="last" colspan=2><div id="param-div" data-dojo-type="dijit/TitlePane" data-dojo-props="title: \'Paramaters\', open:false">'+tableValue+'</div></td></tr>');
+							job_output.push('<tr class="alt"><td class="last" colspan=2><div data-dojo-type="dijit/TitlePane" data-dojo-props="title: \'Paramaters\', open:false">'+tableValue+'</div></td></tr>');
 						} else {
 							job_output.push('<tr class="alt"><th scope="row" style="width:20%"><b>' + this._jobOut[prop]["label"] + '</b></th><td class="last">' + tableValue + "</td></tr>");
 						}
