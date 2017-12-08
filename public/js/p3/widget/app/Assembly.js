7a3796c4f550d713f20657e8d5cd108115b1d0ca
Hyunseung Yoo <hsyoo@anl.gov>
2017-12-08 13:19:50 -0600

    resolves PATRIC3/patric3_website#1787

--- public/js/p3/widget/app/Assembly.js
+++ public/js/p3/widget/app/Assembly.js
@@ -347,7 +347,9 @@ define([
 		onAddSRR: function(){
 			var accession = this.srr_accession.get('value');
 			// console.log("updateSRR", accession, accession.substr(0, 3))
-			if(accession.substr(0, 3) !== 'SRR'){
+			var prefixList = ['SRR', 'ERR']
+			if(prefixList.indexOf(accession.substr(0, 3)) == -1){
+				this.srr_accession.set("state", "Error")
 				return false;
 			}
 
