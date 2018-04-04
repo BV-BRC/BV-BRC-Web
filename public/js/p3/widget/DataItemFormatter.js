de62c276d221ee760b842e89c2081958d3dc43dc
mandywil <mandywil@vt.edu>
2018-04-03 21:37:26 -0400

    Add copy button to copy sequence data to the clipboard.

--- public/js/p3/widget/DataItemFormatter.js
+++ public/js/p3/widget/DataItemFormatter.js
@@ -228,7 +228,8 @@ define([
 				name: 'NA Sequence',
 				text: 'na_sequence',
 				link: function(obj){
-					return obj.na_sequence.substr(0, 30) + '... ' + '<button onclick="window.open(\'/view/FASTA/dna/?in(feature_id,(' + obj.feature_id + '))\')">view</button>';
+					return obj.na_sequence.substr(0, 30) + '... ' + '<button onclick="window.open(\'/view/FASTA/dna/?in(feature_id,(' + obj.feature_id + '))\')">view</button>'
+                                                + '<button onclick="clipboard.copy(\'' + obj.na_sequence + '\')">copy</button>';
 				}
 			}, {
 				name: 'AA Length',
@@ -237,7 +238,8 @@ define([
 				name: 'AA Sequence',
 				text: 'aa_sequence',
 				link: function(obj){
-					return obj.aa_sequence.substr(0, 22) + '... ' + '<button onclick="window.open(\'/view/FASTA/protein/?in(feature_id,(' + obj.feature_id + '))\')">view</button>';
+					return obj.aa_sequence.substr(0, 22) + '... ' + '<button onclick="window.open(\'/view/FASTA/protein/?in(feature_id,(' + obj.feature_id + '))\')">view</button>'
+                                                + '<button onclick="clipboard.copy(\'' + obj.aa_sequence + '\')">copy</button>';
 				}
 			}];
 
