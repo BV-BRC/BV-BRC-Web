435faf846da4d9cdb6bea253d27d97a3937aed82
nconrad <nealconrad@gmail.com>
2018-06-08 17:37:23 -0500

    actually, always display collection date as string from solr

--- public/js/p3/widget/DataItemFormatter.js
+++ public/js/p3/widget/DataItemFormatter.js
@@ -1757,9 +1757,11 @@ define([
         domConstruct.place(renderDataTable(item[key]), td);
         return tr;
       } else if (column.type == 'date') {
-        // display dates as MM/DD/YYYY unless not parseable
+        // display dates as MM/DD/YYYY unless collection date or not parseable
         var d = new Date(item[key]);
-        if (d instanceof Date && !isNaN(d)) {
+        if (key === 'collection_date') {
+          var dateStr = item[key];
+        } else if (d instanceof Date && !isNaN(d)) {
           var dateStr = (d.getMonth() + 1) + '/' + d.getDate() + '/' + d.getFullYear();
         } else {
           var dateStr = item[key];
