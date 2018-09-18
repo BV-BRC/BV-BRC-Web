0d8ee2ffc922f4e41c65d7fb8ae36c0683e3d85d
nconrad <nconrad@anl.gov>
2018-09-18 12:06:45 -0500

    add org labels

--- public/js/p3/widget/viewer/Mauve.js
+++ public/js/p3/widget/viewer/Mauve.js
@@ -1,17 +1,21 @@
 define([
   'dojo/_base/declare', 'dojo/dom-construct', 'dijit/layout/ContentPane',
-  'd3.v5/d3.min', './Base', 'mauve_viewer/dist/mauve-viewer', '../../WorkspaceManager'
+  'd3.v5/d3.min', './Base', 'mauve_viewer/dist/mauve-viewer', '../../WorkspaceManager',
+  'dojo/request', 'dojo/when'
 ], function (
   declare, domConstruct, ContentPane,
-  d3, ViewerBase, MauveViewer, WorkspaceManager
+  d3, ViewerBase, MauveViewer, WorkspaceManager,
+  request, when
 ) {
 
   return declare([ViewerBase], {
-
+    apiServiceUrl: window.App.dataAPI,
     onSetState: function (attr, oldVal, state) {
       if (!state) {
         return;
       }
+      var self = this;
+
       var parts = state.pathname.split('/');
       var path = '/' + parts.slice(2).join('/');
 
@@ -21,7 +25,38 @@ define([
 
       WorkspaceManager.getObject(path).then(function (res) {
         var data = JSON.parse(res.data);
-        new MauveViewer.default({ data: data, ele: container, d3: d3 });
+
+        var ext;
+        var ids = [];
+        data.forEach(function (lcbs) {
+          lcbs.forEach(function (r) {
+            ext = r.name.split('.').pop();
+            var name = r.name.replace(`.${ext}`, '');
+            if (!ids.includes(name)) ids.push(name);
+          });
+        });
+
+        var url = self.apiServiceUrl +
+          'genome/?in(genome_id,(' + ids.join(',') + '))&select(genome_id,genome_name)';
+        when(request.get(url, {
+          headers: {
+            Accept: 'application/json',
+            Authorization: window.App.authorizationToken
+          },
+          handleAs: 'json'
+        }), function (res) {
+          let mapping = {};
+          res.forEach(function (org) {
+            mapping[org.genome_id + '.' + ext] = org.genome_name;
+          });
+
+          new MauveViewer.default({
+            data: data,
+            ele: container,
+            d3: d3,
+            labels: mapping
+          });
+        });
       });
 
       window.document.title = 'PATRIC Mauve Viewer';
