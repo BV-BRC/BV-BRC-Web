903d576a060a88d47130f94f5dc722c465ed55b9
Dustin Machi <dmachi@dmachine.local>
2018-10-30 10:03:32 -0400

    fix back button to home page

--- public/js/p3/app/p3app.js
+++ public/js/p3/app/p3app.js
@@ -135,20 +135,14 @@ define([
         return newState;
       }
 
-      /*
       Router.register("\/$", function(params, oldPath, newPath, state){
         console.log("HOME route", params.newPath);
-        var newState = {href: params.newPath}
-        for (var prop in params.state){
-          newState[prop]=params.state[prop]
+        var homeNode = dom.byId('patric-homepage');
+        if (homeNode) {
+                return;
         }
-
-        newState.widgetClass="dijit/layout/ContentPane";
-        newState.requireAuth=false;
-        console.log("Navigate to ", newState);
-        _self.navigate(newState);
+        window.location.reload();
       });
-      */
 
       Router.register('/remote', function (params, oldPath, newPath, state) {
         console.log('REMOTE WINDOW, WAIT FOR /navigate message');
