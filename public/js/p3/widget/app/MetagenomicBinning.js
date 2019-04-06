9312d016b01a3ff5ead92b84ba23e5a538cc667c
Hyunseung Yoo <hsyoo@anl.gov>
2019-04-06 08:21:15 -0500

    revert application name. this should not be changed without backend change

--- public/js/p3/widget/app/MetagenomicBinning.js
+++ public/js/p3/widget/app/MetagenomicBinning.js
@@ -18,7 +18,7 @@ define([
     baseClass: 'App Assembly',
     pageTitle: 'Metagenomic Binning Service',
     templateString: Template,
-    applicationName: 'MetagenomicBinning',
+    applicationName: 'MetagenomeBinning',
     requireAuth: true,
     applicationLabel: 'Metagenomic Binning',
     applicationDescription: 'The Metagenomic Binning Service accepts either reads or contigs, and attempts to "bin" the data into a set of genomes. This service can be used to reconstruct bacterial and archael genomes from environmental samples.',
