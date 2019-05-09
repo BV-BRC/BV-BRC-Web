40b566efc1671aaf325890d19d28a2e2ae2187e2
Hyunseung Yoo <hsyoo@anl.gov>
2019-05-09 14:30:08 -0500

    bump version 3.5.38

--- public/js/release/p3/widget/JobStatus.js
+++ public/js/release/p3/widget/JobStatus.js
@@ -1,13 +1,19 @@
 require({cache:{
-'url:p3/widget/templates/JobStatus.html':"<div class=\"JobStatusButton\" data-dojo-attach-event=\"onclick:openJobs\">\n  <span>Jobs</span>\n  <span class=\"JobStatusCounts\" data-dojo-attach-point=\"jobStatusTicker\">\n\n    <i class=\"icon-tasks Queued\"></i>\n    <span data-dojo-attach-point=\"jobsQueuedNode\"></span>\n\n    <span class=\"divider\"></span>\n\n    <i class=\"icon-play22 Running\"></i>\n    <span data-dojo-attach-point=\"jobsRunningNode\"></span>\n\n    <span class=\"divider\"></span>\n\n    <i class=\"icon-checkmark2 Completed\"></i>\n    <span data-dojo-attach-point=\"jobsCompleteNode\"></span>\n\n  </span>\n\n  <span data-dojo-attach-point=\"jobsStatusFailed\" class=\"JobStatusCounts Failed\" style=\"display: none; font-weight: bold;\">\n    &nbsp;N/A&nbsp;\n  </span>\n\n</div>\n"}});
+'url:p3/widget/templates/JobStatus.html':"<div class=\"JobStatusButton\" data-dojo-attach-event=\"onclick:openJobs\">\n  <span>Jobs</span>\n  <span class=\"JobStatusCounts\">\n\n    <i class=\"icon-tasks Queued\"></i>\n    <span data-dojo-attach-point=\"jobsQueuedNode\"></span>\n\n    <span class=\"divider\"></span>\n\n    <i class=\"icon-play22 Running\"></i>\n    <span data-dojo-attach-point=\"jobsRunningNode\"></span>\n\n    <span class=\"divider\"></span>\n\n    <i class=\"icon-checkmark2 Completed\"></i>\n    <span data-dojo-attach-point=\"jobsCompleteNode\"></span>\n\n  </span>\n</div>\n"}});
 define("p3/widget/JobStatus", [
-  'dojo/_base/declare', 'dijit/_WidgetBase', 'dojo/topic', 'dojo/_base/lang',
-  'dojo/dom-style',  'dijit/_TemplatedMixin', 'dijit/_WidgetsInTemplateMixin',
-  'dojo/text!./templates/JobStatus.html', 'dijit/layout/ContentPane', 'dijit/Tooltip'
+  'dojo/_base/declare', 'dijit/_WidgetBase', 'dojo/on',
+  'dojo/dom-class', 'dojo/topic', 'dojo/_base/lang',
+  'dojo/dom-construct', '../JobManager',
+  'dijit/_TemplatedMixin', 'dijit/_WidgetsInTemplateMixin',
+  'dojo/text!./templates/JobStatus.html',
+  'dijit/_HasDropDown', 'dijit/layout/ContentPane',
+  'dijit/Tooltip'
 ], function (
-  declare, WidgetBase, Topic, lang,
-  style, TemplatedMixin, WidgetsInTemplate,
-  template, ContentPane, Tooltip
+  declare, WidgetBase, on,
+  domClass, Topic, lang,
+  domConstr, JobManager,
+  TemplatedMixin, WidgetsInTemplate, template,
+  HasDropDown, ContentPane, Tooltip
 ) {
 
   var UploadSummaryPanel = new ContentPane({ content: 'No Active Uploads', style: 'background:#fff;' });
@@ -35,16 +41,6 @@ define("p3/widget/JobStatus", [
     },
     onJobMessage: function (status) {
       if (!status) return;
-
-      if (status == 'failed') {
-        style.set(this.jobStatusTicker, { display: 'none' });
-        style.set(this.jobsStatusFailed, { display: 'inline-block' });
-        return;
-      }
-
-      // restyle on recover from fail
-      style.set(this.jobStatusTicker, { display: 'inline-block' });
-      style.set(this.jobsStatusFailed, { display: 'none' });
 
       this.jobsQueuedNode.innerHTML = status.queued;
       this.jobsRunningNode.innerHTML = status.inProgress;
