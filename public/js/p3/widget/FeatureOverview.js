3ff5149ec18a001b7b25990863ccddee3a62b84d
Chris Thomas <chris.c.thomas@icloud.com>
2020-02-21 13:20:14 -0600

    Update HOGENOM query

--- public/js/p3/widget/FeatureOverview.js
+++ public/js/p3/widget/FeatureOverview.js
@@ -241,9 +241,11 @@ define([
               renderCell: function (obj, val, node) {
                 var baseUrl = formatter.getExternalLinks(obj.id_type);
                 if (obj.id_type.match(/eggNOG/)) {
-                  node.innerHTML = '<a href="' + baseUrl + obj.uniprotkb_accession + '&target_nogs=' + val + '" taget=_blank>' + val + '</a>';
-                } else if (obj.id_type.match(/"HOGENOM|OMA|ProtClustDB"/)) {
-                  node.innerHTML = '<a href="' + baseUrl + obj.uniprotkb_accession + '" taget=_blank>' + val + '</a>';
+                  node.innerHTML = '<a href="' + baseUrl + obj.uniprotkb_accession + '&target_nogs=' + val + '" target=_blank>' + val + '</a>';
+                } else if (obj.id_type.match(/HOGENOM/)) {
+                  node.innerHTML = '<a href="' + baseUrl + obj.uniprotkb_accession + '" target=_blank>' + val + '</a>';
+                } else if (obj.id_type.match(/"OMA|ProtClustDB"/)) {
+                  node.innerHTML = '<a href="' + baseUrl + obj.uniprotkb_accession + '" target=_blank>' + val + '</a>';
                 } else {
                   node.innerHTML = '<a href="' + baseUrl + val + '" target=_blank>' + val + '</a>';
                 }
