3f45ed06ec0d2777c8a22cb42243ce30f7518717
nconrad <1194246+nconrad@users.noreply.github.com>
2020-12-08 18:13:24 -0600

    merge in latest p3web

--- public/js/release/p3/widget/viewer/GenomeAssembly.js
+++
@@ -1,8 +0,0 @@
-define("p3/widget/viewer/GenomeAssembly", [
-  'dojo/_base/declare', './JobResult'
-], function (declare, JobResult) {
-  return declare([JobResult], {
-    containerType: 'GenomeAssembly'
-
-  });
-});
