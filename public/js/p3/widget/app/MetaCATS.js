b10ae95a2b1f1240bff0faaa6b66e8aa8212fbfa
Jacob Porter <jsporter@virginia.edu>
2021-08-12 15:09:06 -0400

    Draft of feature groups interface.

--- public/js/p3/widget/app/MetaCATS.js
+++ public/js/p3/widget/app/MetaCATS.js
@@ -25,10 +25,15 @@ define([
     videoLink: '/videos/',
     pageTitle: 'MetaCATS',
     appBaseURL: 'MetaCATS',
+    startingRows: 10,
+    maxGroups: 10,
     defaultPath: '',
 
     constructor: function () {
       this._selfSet = true;
+      this.addedGroups = 0;
+      this.featureGroupToAttachPt = ['user_genomes_featuregroup'];
+      this.userGenomeList = [];
     },
 
     startup: function () {
@@ -40,33 +45,194 @@ define([
         return;
       }
       this.inherited(arguments);
-
       _self.defaultPath = WorkspaceManager.getDefaultFolder() || _self.activeWorkspacePath;
       _self.output_path.set('value', _self.defaultPath);
+      this.emptyTable(this.groupsTable, this.startingRows);
+      this.numgenomes.startup();
+      this.onInputTypeChange();
       this._started = true;
     },
 
-    validate: function () {
-      if (this.inherited(arguments)) {
-        var p_value = parseFloat(this['p_value'].value);
-        if (p_value && (p_value <= 1 && p_value > 0)) {
-          return true;
-        }
-        return false;
+    onInputTypeChange: function () {
+      if (this.input_groups.checked == true) {
+        this.feature_groups_table.style.display = 'table';
+        this.alignment_files_table.style.display = 'none';
+        this.auto_grouping_table.style.display = 'none';
       }
-      return false;
+      else if (this.input_files.checked == true) {
+        this.feature_groups_table.style.display = 'none';
+        this.alignment_files_table.style.display = 'table';
+        this.auto_grouping_table.style.display = 'none';
+      }
+      else if (this.input_auto.checked == true) {
+        this.feature_groups_table.style.display = 'none';
+        this.alignment_files_table.style.display = 'none';
+        this.auto_grouping_table.style.display = 'table';
+      }
+    },
+
+    ingestAttachPoints: function (input_pts, target, req) {
+      req = typeof req !== 'undefined' ? req : true;
+      var success = 1;
+      input_pts.forEach(function (attachname) {
+        var cur_value = null;
+        if (attachname == 'user_genomes_featuregroup') {
+          cur_value = this[attachname].searchBox.value;
+          var compGenomeList = query('.genomedata');
+          var genomeIds = [];
+          compGenomeList.forEach(function (item) {
+            genomeIds.push(item.genomeRecord.user_genomes_featuregroup);
+          });
+          if (genomeIds.length > 0 && genomeIds.indexOf(cur_value) > -1)  // no same genome ids are allowed
+          {
+            success = 0;
+          }
+        } else {
+          cur_value = this[attachname].value;
+        }
+        if (typeof (cur_value) == 'string') {
+          target[attachname] = cur_value.trim();
+        }
+        else {
+          target[attachname] = cur_value;
+        }
+        this[attachname]._set('state', '');
+        if (target[attachname] != '') {
+          target[attachname] = target[attachname] || undefined;
+        }
+        else if (target[attachname] == 'true') {
+          target[attachname] = true;
+        }
+        else if (target[attachname] == 'false') {
+          target[attachname] = false;
+        }
+      }, this);
+      return (success);
+    },
+
+    emptyTable: function (target, rowLimit) {
+      for (var i = 0; i < rowLimit; i++) {
+        console.log('empty table');
+        var tr = target.insertRow(0);
+        domConstruct.create('td', { innerHTML: "<div class='emptyrow'></div>" }, tr);
+        domConstruct.create('td', { innerHTML: "<div class='emptyrow'></div>" }, tr);
+        domConstruct.create('td', { innerHTML: "<div class='emptyrow'></div>" }, tr);
+      }
+    },
+
+    validate: function () {
+      var ans = this.inherited(arguments);
+      if (ans) {
+        var p_value = parseFloat(this['p_value'].value);
+        if (!(p_value && (p_value <= 1 && p_value > 0))) {
+          ans = false;
+        }
+      }
+      if (ans) {
+        if (this.input_groups.checked == true) {
+          ans = this.numgenomes >= 2;
+        } else if (this.input_files.checked == true) {
+          if (!(this['alignment_file'].value && this['group_file'].value)) {
+            ans = false;
+          }
+        }
+      }
+      if (!ans) {
+        this.submitButton.set('disabled', true);
+      }
+      return ans;
+    },
+
+    makeFeatureGroupName: function () {
+      var name = this.user_genomes_featuregroup.searchBox.get('displayedValue');
+      var maxName = 36;
+      var display_name = name;
+      if (name.length > maxName) {
+        display_name = name.substr(0, (maxName / 2) - 2) + '...' + name.substr((name.length - (maxName / 2)) + 2);
+      }
+      return display_name;
     },
 
     getValues: function () {
       var values = this.inherited(arguments);
-      var alignment_type = null;
-      if (this['alignment_file'].searchBox.onChange.target.item) {
-        alignment_type = this['alignment_file'].searchBox.onChange.target.item.type;
-      }
       values.p_value = parseFloat(this['p_value'].value);
-      values.alignment_type = alignment_type;
+      delete values.user_genomes_featuregroup;
+      if (this.input_groups.checked == true) {
+        values.input_type = 'groups';
+        delete values.alignment_file;
+        delete values.group_file;
+        var compGenomeList = query('.genomedata');
+        var userGroups = [];
+        compGenomeList.forEach(function (item) {
+          if (item.genomeRecord.user_genomes_featuregroup) {
+            userGroups.push(item.genomeRecord.user_genomes_featuregroup);
+          }
+        });
+        values.groups = userGroups;
+      } else if (this.input_files.checked == true) {
+        values.input_type = 'files';
+        var alignment_type = null;
+        if (this['alignment_file'].searchBox.onChange.target.item) {
+          alignment_type = this['alignment_file'].searchBox.onChange.target.item.type;
+        }
+        values.alignment_type = alignment_type;
+      }
       return values;
-    }
+    },
+
+    onAddFeatureGroup: function () {
+      var lrec = {};
+      var chkPassed = this.ingestAttachPoints(this.featureGroupToAttachPt, lrec);
+      if (chkPassed && this.addedGroups < this.maxGroups) {
+        var newGenomeIds = [lrec[this.featureGroupToAttachPt]];
+        if (!newGenomeIds[0]) {
+          return;
+        }
+        var tr = this.groupsTable.insertRow(0);
+        var td = domConstruct.create('td', { 'class': 'textcol genomedata', innerHTML: '' }, tr);
+        td.genomeRecord = lrec;
+        td.innerHTML = "<div class='libraryrow'>" + this.makeFeatureGroupName() + '</div>';
+        domConstruct.create('td', { innerHTML: '' }, tr);
+        var td2 = domConstruct.create('td', { innerHTML: "<i class='fa icon-x fa-1x' />" }, tr);
+        if (this.addedGroups < this.startingRows) {
+          this.groupsTable.deleteRow(-1);
+        }
+        var handle = on(td2, 'click', lang.hitch(this, function (evt) {
+          domConstruct.destroy(tr);
+          this.decreaseGenome();
+          if (this.addedGroups < this.startingRows) {
+            var ntr = this.groupsTable.insertRow(-1);
+            domConstruct.create('td', { innerHTML: "<div class='emptyrow'></div>" }, ntr);
+            domConstruct.create('td', { innerHTML: "<div class='emptyrow'></div>" }, ntr);
+            domConstruct.create('td', { innerHTML: "<div class='emptyrow'></div>" }, ntr);
+          }
+          handle.remove();
+        }));
+        this.increaseGenome();
+      }
+    },
+
+    increaseGenome: function (genomeType, newGenomeIds) {
+      this.addedGroups = this.addedGroups + 1;
+      this.numgenomes.set('value', Number(this.addedGroups));
+      this.validate();
+    },
+
+    decreaseGenome: function () {
+      this.addedGroups = this.addedGroups - 1;
+      this.numgenomes.set('value', Number(this.addedGroups));
+      this.validate();
+    },
+
+    onReset: function (evt) {
+      this.inherited(arguments);
+      for (var i = 0; i < this.addedGroups; i++) {
+        this.groupsTable.deleteRow(0);
+      }
+      this.emptyTable(this.groupsTable, this.addedGroups);
+      this.addedGroups = 0;
+      this.numgenomes.set('value', Number(this.addedGroups));
+    },
 
   });
 });
