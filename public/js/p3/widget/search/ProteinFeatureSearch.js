ca9d190d91b4a93c1a32eff2fac822ecf85600f6
Chris Thomas <chris.c.thomas@icloud.com>
2021-08-31 17:42:08 -0500

    Cleanup Protein Feature Search

--- public/js/p3/widget/search/ProteinFeatureSearch.js
+++ public/js/p3/widget/search/ProteinFeatureSearch.js
@@ -23,23 +23,72 @@ define([
   return declare([SearchBase], {
     templateString: template,
     searchAppName: 'Protein Feature Search',
-    dataKey: 'genome',
-    resultUrlBase: '/view/GenomeList/?',
-    resultUrlHash: '#view_tab=genomes',
+    dataKey: 'protein_feature',
+    resultUrlBase: '/view/ProteinFeaturesList/?',
+    resultUrlHash: '#view_tab',
     postCreate: function () {
       this.inherited(arguments)
 
       this.pathogenGroupNode.store = pathogenGroupStore
+
+      when(storeBuilder('protein_feature', 'source'), lang.hitch(this, function (store) {
+        this.sourceNode.store = store
+      }))
     },
     buildQuery: function () {
       let queryArr = []
+      let genomeQuery = ''
+
+      // genome metadata
+      let genomeQueryArr = []
+
+      const pathogenGroupValue = this.pathogenGroupNode.get('value')
+      if (pathogenGroupValue !== '') {
+        genomeQueryArr.push(`eq(taxon_lineage_ids,${sanitizeInput(pathogenGroupValue)})`)
+      }
+
+      const taxonNameValue = this.taxonNameNode.get('value')
+      if (taxonNameValue !== '') {
+        genomeQueryArr.push(`eq(taxon_lineage_ids,${sanitizeInput(taxonNameValue)})`)
+      }
+
+      if (genomeQueryArr.length > 0) {
+        genomeQuery = `genome(${genomeQueryArr.join(',')})`
+      }
+
+      // protein feature specific search
+      const keywordValue = this.keywordNode.get('value')
+      if (keywordValue !== '') {
+        queryArr.push(`keyword(${sanitizeInput(keywordValue)})`)
+      }
+
+      const sourceValue = this.sourceNode.get('value')
+      if (sourceValue !== '') {
+        queryArr.push(`eq(source,"${sanitizeInput(sourceValue)}")`)
+      }
+
+      const sourceIDValue = this.sourceIDNode.get('value')
+      if (sourceIDValue !== '') {
+        queryArr.push(`eq(source_id,"${sanitizeInput(sourceIDValue)}")`)
+      }
+
+      const descriptionValue = this.descriptionNode.get('value')
+      if (descriptionValue !== '') {
+        queryArr.push(`eq(description,"${sanitizeInput(descriptionValue)}")`)
+      }
 
       const advancedQueryArr = this._buildAdvancedQuery()
       if (advancedQueryArr.length > 0) {
         queryArr = queryArr.concat(advancedQueryArr)
       }
 
-      return queryArr.join('&')
+      const query = queryArr.join('&')
+      if (query !== '') {
+        return query + genomeQuery
+      } else {
+        return query
+      }
+
     }
   })
 })
