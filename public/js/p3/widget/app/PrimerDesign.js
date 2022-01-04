3330e33082b047bd271edfd6ac579651cc57990b
cucinellclark <>
2022-01-04 12:46:08 -0500

    Added validation after selecting a fasta file

--- public/js/p3/widget/app/PrimerDesign.js
+++ public/js/p3/widget/app/PrimerDesign.js
@@ -367,6 +367,7 @@ define([
     //Message to display when selecting a workspace file
     displayNote: function() {
         this.workspace_input_message.innerHTML = 'Note: only the first fasta record will be used';
+        this.validate();
     },
 
     checkBaseParameters: function(values,json_payload) {
