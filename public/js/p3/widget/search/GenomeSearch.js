f599c553281859e53cf04b8780af28ea222a1abc
Chris Thomas <chris.c.thomas@icloud.com>
2022-01-28 10:49:29 -0600

    Add SARS-CoV-2 Criteria Node to Genome Search

--- public/js/p3/widget/search/GenomeSearch.js
+++ public/js/p3/widget/search/GenomeSearch.js
@@ -57,13 +57,23 @@ define([
       storeBuilder('genome', 'season').then(lang.hitch(this, (store) => {
         this.seasonNode.store = store
       }))
+
+      storeBuilder('genome', 'lineage').then(lang.hitch(this, (store) => {
+        this.lineageNode.store = store
+      }))
     },
     onPathogenGroupChange: function () {
       if (this.pathogenGroupNode.get('value') === '11320') {
         this.influenzaCriteriaNode.style.display = 'block'
+        this.sarsCoV2CriteriaNode.style.display = 'none'
+      }
+      else if (this.pathogenGroupNode.get('value') === '2697049') {
+        this.influenzaCriteriaNode.style.display = 'none'
+        this.sarsCoV2CriteriaNode.style.display = 'block'
       }
       else {
         this.influenzaCriteriaNode.style.display = 'none'
+        this.sarsCoV2CriteriaNode.style.display = 'none'
       }
     },
     buildQuery: function () {
@@ -150,6 +160,11 @@ define([
       const seasonValue = this.seasonNode.get('value')
       if (seasonValue !== '') {
         queryArr.push(`eq(season,${sanitizeInput(seasonValue)})`)
+      }
+
+      const lineageValue = this.lineageNode.get('value')
+      if (lineageValue !== '') {
+        queryArr.push(`eq(lineage,${sanitizeInput(lineageValue)})`)
       }
 
       // return queryArr.join('&')
