d24768310da75202d64ae909fca7cebd0fb7588b
Dustin Machi <dmachi@virginia.edu>
2022-08-11 10:39:01 -0400

    Fix user icon update on verifyrefresh page

--- public/js/p3/widget/VerifyEmailRefresh.js
+++ public/js/p3/widget/VerifyEmailRefresh.js
@@ -1,7 +1,7 @@
 define([
-    'dojo/_base/declare', 'dijit/layout/ContentPane'
+    'dojo/_base/declare', 'dijit/layout/ContentPane', 'dojo/dom-class'
   ], function (
-    declare, ContentPane,
+    declare, ContentPane,domClass
   ) {
 
     return declare([ContentPane], {
@@ -9,9 +9,18 @@ define([
         content: '<div style="width:70%;font-size: 2em; margin: auto;text-align:center"><div><i class="icon-user-check fa fa-3x" style="color: green;" ></i></div><div style="border-top: 2px solid green;padding:10px"><p>Thank you for verifying your email address.</p></div></div>',
         startup: function () {
             if (localStorage.getItem("userid") && localStorage.getItem("tokenstring")){
-                console.log('do user refresh')
                 window.App.refreshUser().then(function(){
-                    console.log("User Refresh Completed")
+                    var rawu = localStorage.getItem("userProfile")
+                    var u = JSON.parse(rawu)
+                    if (!u) {
+                      console.log("Missing User: ", rawu)
+                      return
+                    }
+                    if (u.email_verified){
+                      domClass.remove(document.body,"unverified_email")
+                    }else{
+                      domClass.add(document.body,"unverified_email")
+                    }
                 })
             }
         }
