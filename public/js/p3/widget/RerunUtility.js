4c20187a51665ede0e4e60300f9a1f4b2dd09cde
cucinellclark <>
2023-02-16 11:32:55 -0500

    adding msa to rerun

--- public/js/p3/widget/RerunUtility.js
+++ public/js/p3/widget/RerunUtility.js
@@ -2,7 +2,7 @@ define([], function () {
   return {
 
     rerun: function (job_params, service_id, window, Topic) {
-      // TODO: make sure service_id variable is present for every service
+      // make sure service_id variable is present for every service
       var sessionStorage = window.sessionStorage;
       const random = (length = 8) => {
         return Math.random().toString(16).substr(2, length);
@@ -22,16 +22,14 @@ define([], function () {
         'GenomeAssembly2': 'Assembly2',
         'GenomeAssembly': 'Assembly2',
         'GenomeAlignment': 'GenomeAlignment',
-        // TODO: rerun for annotation needs to be updated
         'GenomeAnnotation': 'Annotation',
         // 'GenomeComparison': 'SeqComparison',
         'Homology': 'Homology',
         // 'MetaCATS': 'MetaCATS',
         'MetagenomeBinning': 'MetagenomicBinning',
         'MetagenomicReadMapping': 'MetagenomicReadMapping',
-        // 'MSA': 'MSA',
+        'MSA': 'MSA',
         'CodonTree': 'PhylogeneticTree',
-        // TODO: need to fix this
         'PrimerDesign': 'PrimerDesign',
         'RNASeq': 'Rnaseq',
         'SubspeciesClassification': 'SubspeciesClassification',
@@ -40,7 +38,7 @@ define([], function () {
         'Variation': 'Variation'
       };
       if (service_app_map.hasOwnProperty(service_id)) {
-        Topic.publish('/navigate', { href: '/app/' + service_app_map[service_id] + '?rerun_key=' + rerun_key, target: 'blank' });
+        Topic.publish('/navigate', { href: '/app/' + service_app_map[service_id] + '?rerun_key=' + rerun_key });
       }
       else {
         console.log('Rerun not enabled for: ', service_id);
