5de1e3e96e61b2f631333b93c398b6f9b6379746
cucinellclark <>
2023-10-12 11:57:48 -0700

    adding banner to archaeopteryx phylogeny

--- public/js/p3/widget/Phylogeny.js
+++ public/js/p3/widget/Phylogeny.js
@@ -62,7 +62,7 @@ define([
         region: 'top',
         splitter: false,
         content: '',
-        style: 'height: 20px; margin:0px;padding:0px; overflow: hidden; vertical-align:middle;',
+        style: 'height: 30px;display: flex;align-items:center; margin:0px;padding:0px; overflow: hidden; vertical-align:middle;',
         className: 'TextTabButtons'
       });
       this.selectionActionBar = new ActionBar({
@@ -265,6 +265,25 @@ define([
         var phyloxml_file = data[taxonId];
         phyloxml_file = 'https://www.bv-brc.org/api/content/bvbrc_phylogeny_tab/phyloxml/' + phyloxml_file;
         request.get(phyloxml_file).then(lang.hitch(this, function (phyloxml) {
+          /*
+          <div name="overview" class="infobox iconbox infobutton dialoginfo">
+          <i class="fa icon-info-circle fa" title="click to open info dialog"></i>
+        </div>
+          */
+          var iconDiv = domConstruct.create('div', {
+            name: 'overview',
+            class: 'infobox iconbox infobutton dialoginfo',
+            innerHTML: '<i class="fa icon-info-circle fa" title="click to open info dialog"></i>'
+          });
+          var file_parts = phyloxml_file.split('/').reverse()[0].split('_');
+          var taxon_level = file_parts[2];
+          var taxon_name = file_parts[0];
+          var phylo_title = 'GTDB Reference Phylogeny for ' + taxon_level + ': ' + taxon_name;
+          var taxonDiv = domConstruct.create('div', { style: 'padding-top: 20px;padding-left: 5px', innerHTML: '<h5>' + phylo_title + '</h5>' })
+          // this.containerActionBar.set('value', '<p>'+phyloxml_file+'</p>');
+          // this.containerActionBar.set('innerHTML', cabDiv);
+          domConstruct.place(iconDiv, this.containerActionBar.domNode, 'last');
+          domConstruct.place(taxonDiv, this.containerActionBar.domNode, 'last');
           var tree;
           var options = {};
           var settings = this.settings;
