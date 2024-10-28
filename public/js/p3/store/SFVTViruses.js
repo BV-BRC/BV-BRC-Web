32166de61aafcb49610ce85e0505d3db2752e329
Mehmet Kuscuoglu <kscglu@gmail.com>
2024-10-28 09:07:05 -0700

    hide SFVT for dengue

--- public/js/p3/store/SFVTViruses.js
+++ public/js/p3/store/SFVTViruses.js
@@ -6,7 +6,7 @@ define([
 
   return new Memory({
     data: [
-      {name: 'Dengue virus', id: 12637},
+      /*{name: 'Dengue virus', id: 12637},*/
       {name: 'Influenza A virus', id: 11320},
       {name: 'Monkeypox virus', id: 10244}
     ]
